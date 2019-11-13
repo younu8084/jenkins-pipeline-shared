258fc39e7b961874b2715f9733cf356d2dd98e9e
REVATHISARAISWATHI <36625520+REVATHISARAISWATHI@users.noreply.github.com>
2019-11-12 19:49:25 -0800

    Update sonar.groovy

--- vars/sonar.groovy
+++ vars/sonar.groovy
@@ -5,7 +5,7 @@ def call(String buildStatus = 'SONARSTARTED') {
   def details = """<p>${buildStatus}: Job '${env.JOB_NAME} [${env.BUILD_NUMBER}]':</p>
     <p>Check console output at &QUOT;<a href='${env.BUILD_URL}'>${env.JOB_NAME} [${env.BUILD_NUMBER}]</a>&QUOT;</p>"""
   
-  if (buildStatus == 'STARTEDNEX') {
+  if (buildStatus == 'SONARSTARTED') {
     color = 'BLUE'
     colorCode = '#1eaee3'
     slackSend (color: colorCode, message: summary)
