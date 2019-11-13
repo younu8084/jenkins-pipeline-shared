3a7c49129c918ba3f4c2d8f1acd80f42cb3ad540
REVATHISARAISWATHI <36625520+REVATHISARAISWATHI@users.noreply.github.com>
2019-11-13 17:57:23 +0530

    dev

--- vars/deploy_development.groovy
+++ vars/deploy_development.groovy
@@ -1,1 +1,5 @@
+def call(String msg = 'deploy_dev') {
+echo "${msg}"
+deploy adapters: [tomcat8(credentialsId: 'tomcat', path: '', url: 'http://18.224.251.223:8000/')], contextPath: null, onFailure: false, war: '**/*.war'
+            }
 
