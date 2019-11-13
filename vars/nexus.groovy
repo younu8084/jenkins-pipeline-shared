4f6b4464598d6c5762b725391944d4eddf73aed2
REVATHISARAISWATHI <36625520+REVATHISARAISWATHI@users.noreply.github.com>
2019-11-13 13:09:19 +0530

    Update nexus.groovy

--- vars/nexus.groovy
+++ vars/nexus.groovy
@@ -1,11 +1,11 @@
-def call(String buildStatus = 'NEXUSSTARTED') {
+def call(String buildStatus = 'NEXUS STAGE STARTED') {
   // build status of null means successful
   def subject = "${buildStatus}: Job '${env.JOB_NAME} [${env.BUILD_NUMBER}]'"
   def summary = "${subject} (${env.BUILD_URL})"
   def details = """<p>${buildStatus}: Job '${env.JOB_NAME} [${env.BUILD_NUMBER}]':</p>
     <p>Check console output at &QUOT;<a href='${env.BUILD_URL}'>${env.JOB_NAME} [${env.BUILD_NUMBER}]</a>&QUOT;</p>"""
   
-  if (buildStatus == 'NEXUSSTARTED') {
+  if (buildStatus == 'NEXUS STAGE STARTED') {
     color = 'NBLUE'
     colorCode = '#032cfc'
     slackSend (color: colorCode, message: summary)
