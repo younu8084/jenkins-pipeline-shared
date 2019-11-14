4d40f9e732105d2ae6bbcd46ab1f3b1d5e785fff
REVATHISARAISWATHI <36625520+REVATHISARAISWATHI@users.noreply.github.com>
2019-11-14 14:19:41 +0530

    Update approval.groovy

--- vars/approval.groovy
+++ vars/approval.groovy
@@ -19,3 +19,24 @@ echo "${msg}"
         echo ("submitted by: "+userInput['submitter'])
                       }
                       }
+def call(String msg = 'APPROVAL1') {
+echo "${msg}"
+                      script{
+                         emailext mimeType: 'text/html',
+                 subject: "[Jenkins]${currentBuild.fullDisplayName}",
+                 to: "abhishek.gupta2015a@gmail.com",
+                 body: '''<a href="http://3.14.72.144:8080/">click to approve</a>'''
+
+        def userInput = input id: 'userInput',
+                              message: 'Let\'s promote?', 
+                              submitterParameter: 'submitter',
+                              submitter: 'Abhishek',
+                              parameters: [
+                                [$class: 'TextParameterDefinition', defaultValue: 'sit', description: 'Environment', name: 'env'],
+                                [$class: 'TextParameterDefinition', defaultValue: 'k8s', description: 'Target', name: 'target']]
+
+        echo ("Env: "+userInput['env'])
+        echo ("Target: "+userInput['target'])
+        echo ("submitted by: "+userInput['submitter'])
+                      }
+                      }
