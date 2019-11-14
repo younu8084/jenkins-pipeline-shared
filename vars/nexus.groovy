c78cecba3fdc3d59c6b5b8f8f88d4d9cbb941c1d
REVATHISARAISWATHI <36625520+REVATHISARAISWATHI@users.noreply.github.com>
2019-11-14 13:00:52 +0530

    Update nexus.groovy

--- vars/nexus.groovy
+++ vars/nexus.groovy
@@ -1,6 +1,6 @@
 def call(String msg = 'NEXUS') {
 echo "${msg}"
        withCredentials([usernamePassword(credentialsId: 'nexus_creds', passwordVariable: 'password', usernameVariable:'username')]) {
-              sh 'curl -u ${username}:${password} --upload-file target/springboot-1.0.0-SNAPSHOT-$BUILD_NUMBER.war http://ec2-18-224-155-110.us-east-2.compute.amazonaws.com:8081/nexus/content/repositories/devopstraining/hexagon6/'
+              sh 'curl -u ${username}:${password} --upload-file target/springboot-1.0.0-SNAPSHOT.war http://ec2-18-224-155-110.us-east-2.compute.amazonaws.com:8081/nexus/content/repositories/devopstraining/hexagon6/springboot-1.0.0-SNAPSHOT-$BUILD_NUMBER.war'
          }
 }
