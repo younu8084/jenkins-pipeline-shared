2b079f50cd0e6faa1d324c9ca8ac017d6e1c23d7
REVATHISARAISWATHI <36625520+REVATHISARAISWATHI@users.noreply.github.com>
2019-11-14 13:17:27 +0530

    Update deploy_development.groovy

--- vars/deploy_development.groovy
+++ vars/deploy_development.groovy
@@ -1,5 +1,5 @@
 def call(String msg = 'deploy_dev') {
 echo "${msg}"
-deploy adapters: [tomcat8(credentialsId: 'tomcat', path: '', url: 'http://18.224.251.223:8000/')], contextPath: null, onFailure: false, war: '**/*.war'
+deploy adapters: [tomcat8(credentialsId: 'tomcat', path: '', url: 'http://3.14.72.144:8000/')], contextPath: null, onFailure: false, war: '**/*.war'
             }
 
