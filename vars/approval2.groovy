97d5fe7fc41fd4b152aa1e23e65ff645af8b7e81
REVATHISARAISWATHI <36625520+REVATHISARAISWATHI@users.noreply.github.com>
2019-11-14 19:01:45 +0530

    Update approval2.groovy

--- vars/approval2.groovy
+++ vars/approval2.groovy
@@ -4,7 +4,7 @@ echo "${msg}"
                          emailext mimeType: 'text/html',
                  subject: "[Jenkins]${currentBuild.fullDisplayName}",
                  to: "revathims1998@gmail.com",
-                 body: '''<a href="http://18.221.174.168:8080/">click to approve</a>'''
+                 body: '''<a href="http://3.134.99.123:8080/">click to approve</a>'''
 
         def userInput = input id: 'userInput',
                               message: 'Let\'s promote?', 
