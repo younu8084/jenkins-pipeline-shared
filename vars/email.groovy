0104c23b6a422d0aa221b7ab49f0e8d174757a66
REVATHISARAISWATHI <36625520+REVATHISARAISWATHI@users.noreply.github.com>
2019-11-14 11:43:37 +0530

    Create email.groovy

---
+++ vars/email.groovy
@@ -0,0 +1,11 @@
+def call(String mail) {
+def mailRecipients="mail"
+ emailext (
+      to: mailRecipients,
+      replyTo: mailRecipients,
+      subject: subject,
+      body: details,
+      recipientProviders: [[$class: 'DevelopersRecipientProvider']]
+     //recipientProviders: [[$class: 'RequesterRecipientProvider']]
+    )
+    }
