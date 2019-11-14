eef79dd232a349f26d2cdbf583eba147dd0ad5e3
REVATHISARAISWATHI <36625520+REVATHISARAISWATHI@users.noreply.github.com>
2019-11-14 17:44:57 +0530

    Update functional.groovy

--- vars/functional.groovy
+++ vars/functional.groovy
@@ -1,6 +1,6 @@
 def call(String msg = 'functional_test') {
 echo "${msg}"
  
-      blazeMeterTest credentialsId: 'NEWONE', getJtl: true, getJunit: true, testId: '7425961.functionalGui', workspaceId: '390025'
+     blazeMeterTest credentialsId: 'NEWONE', getJtl: true, getJunit: true, testId: '7422114.functionalApi', workspaceId: '390025'
     
     }
