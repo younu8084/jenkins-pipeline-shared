dc73d1d17af6a0cd1284ee082bed36c81ebe3382
REVATHISARAISWATHI <36625520+REVATHISARAISWATHI@users.noreply.github.com>
2019-11-14 14:33:41 +0530

    Update scan.groovy

--- vars/scan.groovy
+++ vars/scan.groovy
@@ -1,4 +1,4 @@
 def call(String msg = 'SCAN') {
 echo "${msg}"
-appscan application: '49f54256-de9f-4a37-9fee-a836311ee015', credentials: 'IBM CREDENTIALS', email: true, failBuild: true, failureConditions: [failure_condition(failureType: 'high', threshold: 100), failure_condition(failureType: 'total', threshold: 100)], name: 'IBM Security', scanner: static_analyzer(hasOptions: false, target: '/var/lib/jenkins/workspace/'), type: 'Static Analyzer', wait: true
+appscan application: '49f54256-de9f-4a37-9fee-a836311ee015', credentials: 'IBM CREDENTIALS', email: true, failBuild: true, failureConditions: [failure_condition(failureType: 'high', threshold: 1000), failure_condition(failureType: 'total', threshold: 1000)], name: 'IBM Security', scanner: static_analyzer(hasOptions: false, target: '/var/lib/jenkins/workspace/'), type: 'Static Analyzer', wait: true
 }
