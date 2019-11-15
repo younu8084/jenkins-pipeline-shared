796fa2e566b3d9f402a2669ecfbe0652038c0481
REVATHISARAISWATHI <36625520+REVATHISARAISWATHI@users.noreply.github.com>
2019-11-15 14:25:26 +0530

    Update deploy_ansible.groovy

--- vars/deploy_ansible.groovy
+++ vars/deploy_ansible.groovy
@@ -1,4 +1,9 @@
 def call(String msg = 'deploy_ansible') {
 echo "${msg}"
-  sh 'scp -i /var/lib/jenkins/.ssh/id_rsa -r /var/lib/jenkins/workspace/springboot-demo/target/springboot-0.0.1-SNAPSHOT.war ansadmin@172.31.31.91:/projects'
+  when{ branch "developer"}
+  sh 'scp -i /var/lib/jenkins/.ssh/id_rsa -r /var/lib/jenkins/workspace/springboot-demo_developer/target/springboot-0.0.1-SNAPSHOT.war ansadmin@172.31.31.91:/projects/developer/playfile.yml'
+  when{ branch "master"}
+  sh 'scp -i /var/lib/jenkins/.ssh/id_rsa -r /var/lib/jenkins/workspace/springboot-demo_master/target/springboot-0.0.1-SNAPSHOT.war ansadmin@172.31.31.91:/projects/developer/performance.yml'
+  when{ branch "release"}
+  sh 'scp -i /var/lib/jenkins/.ssh/id_rsa -r /var/lib/jenkins/workspace/springboot-demo_release/target/springboot-0.0.1-SNAPSHOT.war ansadmin@172.31.31.91:/projects/developer/production.yml'
 }
