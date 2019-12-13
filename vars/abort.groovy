ca27183c665f7a2d0d49dd7803b1efeab0c432e4
REVATHISARAISWATHI <36625520+REVATHISARAISWATHI@users.noreply.github.com>
2019-12-13 19:08:09 +0530

    Update abort.groovy

--- vars/abort.groovy
+++ vars/abort.groovy
@@ -9,7 +9,7 @@ def call(body) {
     
     while (previousBuild != null) {
         
-        if (d>0.2) {
+        if (d>0.1) {
             def executor = previousBuild.getExecutor()
             if (executor != null) {
                 echo ">> Aborting older build #${previousBuild.number}."
