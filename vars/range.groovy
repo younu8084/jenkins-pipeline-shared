43f08db0ccba414fb3640ec56f0cda95690bc498
REVATHISARAISWATHI <36625520+REVATHISARAISWATHI@users.noreply.github.com>
2019-12-16 15:16:23 +0530

    Update range.groovy

--- vars/range.groovy
+++ vars/range.groovy
@@ -1,6 +1,7 @@
 import jenkins.model.*;
 import hudson.model.Fingerprint.RangeSet;
 def call(body) {
+ def jobName = "${env.JOB_NAME}"
 def j = jenkins.model.Jenkins.instance.getItem(jobName);
 
 def r = RangeSet.fromString(buildRange, true);
