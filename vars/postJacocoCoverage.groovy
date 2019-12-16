3439412c35eee600a470fd3472a34aa94143e7ba
REVATHISARAISWATHI <36625520+REVATHISARAISWATHI@users.noreply.github.com>
2019-12-16 11:52:53 +0530

    Create postJacocoCoverage.groovy

---
+++ vars/postJacocoCoverage.groovy
@@ -0,0 +1,15 @@
+#!/usr/bin/env groovy
+
+def call(Map args) {
+  final Double coverageThreshold = args.threshold ?: 80.0
+  final Double coverageDeltaThreshold = args.deltaThreshold ?: null
+  final String xmlPath = args.xmlPath ?: "target/site/jacoco/jacoco.xml"
+
+  final lib = new com.spotify.jenkinsfile.Coverage()
+
+  final Double coverage = lib.getCoverageFromJacoco(xmlPath)
+  lib.postCoverage(coverage, coverageThreshold)
+
+  final Double coverageDelta = lib.getCoverageDelta()
+  lib.postCoverageDelta(coverageDelta, coverageDeltaThreshold)
+}
