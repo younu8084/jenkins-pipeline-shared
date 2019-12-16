d7f33f987f0851f98877e8c4f4e788a09e97f2b2
REVATHISARAISWATHI <36625520+REVATHISARAISWATHI@users.noreply.github.com>
2019-12-16 14:40:16 +0530

    Update deletera.groovy

--- vars/deletera.groovy
+++ vars/deletera.groovy
@@ -1,6 +1,7 @@
 import jenkins.model.*
- jenkins = Jenkins.instance
+ 
  def delete(String name,int s,int e)
 {
-Jenkins.instance.getItemByFullName('$name').builds.findAll { it.number > $s && it.number < $e }.each { it.delete() }
+ jenkins = Jenkins.instance
+Jenkins.getItemByFullName('$name').builds.findAll { it.number > $s && it.number < $e }.each { it.delete() }
 }
