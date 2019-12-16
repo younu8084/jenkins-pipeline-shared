01a5fc665ec736c872da83b6362f51490cb97911
REVATHISARAISWATHI <36625520+REVATHISARAISWATHI@users.noreply.github.com>
2019-12-16 01:52:07 -0800

    Update deletelist.groovy

--- vars/deletelist.groovy
+++ vars/deletelist.groovy
@@ -8,7 +8,7 @@ def dryRun = true
 
 Jenkins.get().getAllItems(WorkflowMultiBranchProject.class).each { WorkflowMultiBranchProject job ->
   if (dryRun) {
-      println "${free1} will be removed."
+      println "${job.free1} will be removed."
   } 
 }
 return
