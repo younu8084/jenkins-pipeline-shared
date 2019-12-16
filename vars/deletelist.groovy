2a2eace78b1ab5d145f79a062be17f30d3f59094
REVATHISARAISWATHI <36625520+REVATHISARAISWATHI@users.noreply.github.com>
2019-12-16 01:42:10 -0800

    Create deletelist.groovy

---
+++ vars/deletelist.groovy
@@ -0,0 +1,14 @@
+import jenkins.model.Jenkins
+import hudson.model.Job
+import org.jenkinsci.plugins.workflow.multibranch.WorkflowMultiBranchProject
+
+//Input:
+//If dryRun is true, will print list of jobs that would be included in the deletion
+def dryRun = true
+
+Jenkins.get().getAllItems(WorkflowMultiBranchProject.class).each { WorkflowMultiBranchProject job ->
+  if (dryRun) {
+      println "${job.fullName} will be removed."
+  } 
+}
+return
