9515c33c548c1d16438d9e937416889dd370d649
REVATHISARAISWATHI <36625520+REVATHISARAISWATHI@users.noreply.github.com>
2019-12-16 14:20:39 +0530

    Update deletera.groovy

--- vars/deletera.groovy
+++ vars/deletera.groovy
@@ -3,3 +3,6 @@ def call(String name='jobname',int s,int e)
 
 Jenkins.instance.getItemByFullName('$msg').builds.findAll { it.number > $s && it.number < $e }.each { it.delete() }
 }
+return[
+  call: this.&call
+  ]
