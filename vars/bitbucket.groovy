6ee3ac911cc3516f2a452dfe26350c30e982d8be
REVATHISARAISWATHI <36625520+REVATHISARAISWATHI@users.noreply.github.com>
2020-01-22 15:53:56 +0530

    Update bitbucket.groovy

--- vars/bitbucket.groovy
+++ vars/bitbucket.groovy
@@ -1,5 +1,5 @@
 def call(){
- sh 'curl -X POST https://Megalai:KAcuULzqy2ZcELHKMNH08A4F@api.bitbucket.org/2.0/repositories/Megalai1/demo1'
+ sh 'curl -X POST https://Megalai:KAcuULzqy2ZcELHKMNH08A4F@api.bitbucket.org/2.0/repositories/Megalai1/demo1 '-d "name=project_name"''
 }
 
       
