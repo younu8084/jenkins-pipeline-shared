fbe4bd50722028510fa2c8a11a00eec0dbc8b48e
REVATHISARAISWATHI <36625520+REVATHISARAISWATHI@users.noreply.github.com>
2020-01-21 22:08:39 -0800

    Update bitbucket.groovy

--- vars/bitbucket.groovy
+++ vars/bitbucket.groovy
@@ -1,3 +1,3 @@
 def call(){
-sh curl -X POST -v -u ${username}:${password} "https://api.bitbucket.org/2.0/repositories/Megalai" -H "Content-Type: application/json"  -d '{"has_wiki": true, "is_private": true, "project": {"key": "PRJ_KEY"}}'
+sh curl -X POST -v -u ${username}:${password} "https://api.bitbucket.org/2.0/repositories/Megalai" -H "Content-Type: application/json"  -d '{"is_private": true, "project": {"key": "PRJ_KEY"}}'
 }
