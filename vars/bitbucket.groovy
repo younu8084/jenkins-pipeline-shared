f7c41e96c942bac70ab694c96cbc993487de01cc
REVATHISARAISWATHI <36625520+REVATHISARAISWATHI@users.noreply.github.com>
2020-01-22 01:27:11 -0800

    Update bitbucket.groovy

--- vars/bitbucket.groovy
+++ vars/bitbucket.groovy
@@ -1,10 +1,10 @@
 def call(){
-   curl -X POST -H "Content-Type: application/json" -d '{
+ sh 'curl -X POST -H "Content-Type: application/json" -d '{
     "scm": "git",
     "project": {
         "key": "DEM"
     }
-}' https://api.bitbucket.org/2.0/megalai1/projects/DEM/repositories/repo1
+}' https://api.bitbucket.org/2.0/megalai1/projects/DEM/repositories/repo1'
 }
 
       
