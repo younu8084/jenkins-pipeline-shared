3f2daf35edd36dcc6a8f3f37c44a2e96f5525458
REVATHISARAISWATHI <36625520+REVATHISARAISWATHI@users.noreply.github.com>
2020-01-22 22:29:38 +0530

    Update bitbucket.groovy

--- vars/bitbucket.groovy
+++ vars/bitbucket.groovy
@@ -1,6 +1,6 @@
 def call(){
 
-sh 'curl --user Megalai:Mumani1209@98 https://api.bitbucket.org/2.0/repositories/mani123_/task1 --data name=REPO_NAME '
+sh 'curl -X POST --user Megalai:Mumani1209@98 https://api.bitbucket.org/2.0/repositories/mani123_/task1 --data name=REPO_NAME '
 }
 
       
