fd1d2101ffe6b379101314c6875a20323413d50e
REVATHISARAISWATHI <36625520+REVATHISARAISWATHI@users.noreply.github.com>
2020-01-22 17:35:30 +0530

    Update bitbucket.groovy

--- vars/bitbucket.groovy
+++ vars/bitbucket.groovy
@@ -1,6 +1,6 @@
 def call(){
 
-sh 'curl -X POST -v -user Megalai:Mumani1209@98 "https://api.bitbucket.org/2.0/repositories/Megalai1/demo/repo"'
+sh 'curl --user bbdocs:bbpassword https://api.bitbucket.org/2.0/repositories/Megalai'
 }
 
       
