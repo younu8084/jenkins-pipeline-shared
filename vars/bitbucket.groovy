ebbc6d5741ac36045940bcc98d1d76c6fe0b2ee3
REVATHISARAISWATHI <36625520+REVATHISARAISWATHI@users.noreply.github.com>
2020-01-22 12:44:12 +0530

    Update bitbucket.groovy

--- vars/bitbucket.groovy
+++ vars/bitbucket.groovy
@@ -1,4 +1,4 @@
 def call(){
-   sh 'curl -X POST -v -user Megalai:Mumani1209@98 "https://api.bitbucket.org/2.0/repositories/Megalai1/repo1" -d "scm=git", "is_private=true", "fork_policy=no_public_forks"'
+   sh 'curl -X POST -v -user Megalai:Mumani1209@98 "https://api.bitbucket.org/2.0/repositories/Megalai1/repo1" -d "scm=git", "is_private=true"'
 }
 
