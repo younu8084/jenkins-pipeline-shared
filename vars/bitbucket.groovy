824759d68cdbbb2e629cdf3c44bdef5fba292f50
REVATHISARAISWATHI <36625520+REVATHISARAISWATHI@users.noreply.github.com>
2020-01-22 12:31:03 +0530

    Update bitbucket.groovy

--- vars/bitbucket.groovy
+++ vars/bitbucket.groovy
@@ -1,3 +1,14 @@
-def call(){
-sh 'curl -k -X POST --user Megalai:Mumani1209@98 "https://api.bitbucket.org/2.0/repositories/megalai1" -d "name=project_name"'
+def call(){curl -u Megalai:Mumani1209@98  -X POST -H "Content-Type: application/json" -d '{
+
+   "slug": "test",
+
+   "forkable": false,
+
+   "project": {
+
+        "key": "DEMO"
+
+    }
+
+}' https://bitbucket.org.com/rest/api/2.0/projects/demo/repos/test
 }
