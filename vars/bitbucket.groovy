0ddea959bc178f47ddd3c6f2baf48b1ec292ace3
REVATHISARAISWATHI <36625520+REVATHISARAISWATHI@users.noreply.github.com>
2020-01-23 08:40:33 -0800

    Update bitbucket.groovy

--- vars/bitbucket.groovy
+++ vars/bitbucket.groovy
@@ -2,7 +2,7 @@ import jenkins.model.*
  jenkins = Jenkins.instance
 def call(String name ="name"){
 
-      sh 'curl -X POST -v --user "${name}":admin123 -H "ContentType: application/json; charset=UTF-8" "https://api.bitbucket.org/2.0/repositories/Megalai/repo7" -d {"name":"repo7"}'
+      sh 'curl -X POST -v --user "${name}":admin123 -H "ContentType: application/json; charset=UTF-8" "https://api.bitbucket.org/2.0/repositories/Megalai/repo8" -d {"name":"repo8"}'
 
 }
 
