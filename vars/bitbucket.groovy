e59cec415e87018fe2e6aa241c193ae844dffb22
REVATHISARAISWATHI <36625520+REVATHISARAISWATHI@users.noreply.github.com>
2020-01-23 12:31:18 +0530

    Update bitbucket.groovy

--- vars/bitbucket.groovy
+++ vars/bitbucket.groovy
@@ -1,6 +1,6 @@
 def call(){
 
-sh 'curl -X POST authentication: 'Bitbucket', contentType: 'APPLICATION_JSON', "https://api.bitbucket.org/2.0/repositories/Megalai/epo3" -d {"name":"epo3"}'
+sh 'curl -X POST authentication: '$BITBUCKET_COMMON_CREDS', contentType: 'APPLICATION_JSON', "https://api.bitbucket.org/2.0/repositories/Megalai/epo3" -d {"name":"epo3"}'
 
 }
 
