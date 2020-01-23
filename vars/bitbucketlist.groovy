3f80354772cbae1392a12bddcc0f53f2d7b93bef
REVATHISARAISWATHI <36625520+REVATHISARAISWATHI@users.noreply.github.com>
2020-01-23 05:05:39 -0800

    Update bitbucketlist.groovy

--- vars/bitbucketlist.groovy
+++ vars/bitbucketlist.groovy
@@ -1,5 +1,5 @@
 def call(){
 
-sh 'curl -v --user Megalai:Mumani1209@98 -H "ContentType: application/json; charset=UTF-8"  https://api.bitbucket.org/2.0/repositories/Megalai'
+sh 'curl --user Megalai:admin123 https://api.bitbucket.org/2.0/repositories/Megalai'
 
 }
