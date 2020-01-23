f3dbe84f1e02500c744b2d83da9d25d124c871ce
REVATHISARAISWATHI <36625520+REVATHISARAISWATHI@users.noreply.github.com>
2020-01-23 01:52:47 -0800

    Update bitbucket.groovy

--- vars/bitbucket.groovy
+++ vars/bitbucket.groovy
@@ -1,6 +1,6 @@
 def call(){
 
-sh 'curl -X POST --user Megalai:Mumani1209@98 -H "Content-Type: application/json" "https://api.bitbucket.org/2.0/repositories/Megalai/repo4" -d {"name":"repo4"}'
+sh 'curl -H "Content-Type: application/json" -X POST --user Megalai:Mumani1209@98 "https://api.bitbucket.org/2.0/repositories/Megalai/repo4" -d {"name":"repo4"}'
 
 }
 
