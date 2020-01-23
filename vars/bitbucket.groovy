65257c6b786d218e3c3474152a0936fb53615274
REVATHISARAISWATHI <36625520+REVATHISARAISWATHI@users.noreply.github.com>
2020-01-22 23:39:29 -0800

    Update bitbucket.groovy

--- vars/bitbucket.groovy
+++ vars/bitbucket.groovy
@@ -1,6 +1,6 @@
 def call(){
 
-sh 'curl -X POST --user '$BITBUCKET_COMMON_CREDS' "https://api.bitbucket.org/2.0/repositories/Megalai/epo3" -d {"name":"epo3"}'
+sh 'curl -X POST --user Megalai:Mumani1209@98 "https://api.bitbucket.org/2.0/repositories/Megalai/repo3" -d {"name":"repo3"}'
 
 }
 
