88a1e3010944276685a9b1ddb766a0ec65cc16ef
REVATHISARAISWATHI <36625520+REVATHISARAISWATHI@users.noreply.github.com>
2020-01-24 17:58:29 +0530

    Update nexusconector.groovy

--- vars/nexusconector.groovy
+++ vars/nexusconector.groovy
@@ -1,4 +1,4 @@
 def call()
 {
-sh 'curl -X POST -i -H  "Accept: application/json" -H "Content-Type: application/json" -trace-ascii -d "@repo.json" -u admin:admin123 "http://3.15.18.214:8081/nexus/service/local/repositories"'
+sh 'curl -X POST -i -H  "Accept: application/json" -H "Content-Type: application/json" -d "@repo.json" -u admin:admin123 "http://3.15.18.214:8081/nexus/service/local/repositories"'
 }
