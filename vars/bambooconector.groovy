ff8771c8ef7b70d419017d6ad552232e107f2468
REVATHISARAISWATHI <36625520+REVATHISARAISWATHI@users.noreply.github.com>
2020-01-26 10:56:45 +0530

    Update bambooconector.groovy

--- vars/bambooconector.groovy
+++ vars/bambooconector.groovy
@@ -1,4 +1,4 @@
 def call()
 {
- sh 'curl -X POST --user rig:rigaDapt@devOps "http://18.220.143.53:8085/rest/api/latest/queue/BAM-WEEK?executeAllStages=true"'
+ sh 'curl -X POST --user un:pwd "http://18.220.143.53:8085/rest/api/latest/queue/BAM-WEEK?executeAllStages=true"'
 }
