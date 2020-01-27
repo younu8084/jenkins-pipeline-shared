3fada9cd5d89688245341c916f0951d11493b877
REVATHISARAISWATHI <36625520+REVATHISARAISWATHI@users.noreply.github.com>
2020-01-27 16:31:08 +0530

    Update createRepo.groovy

--- vars/createRepo.groovy
+++ vars/createRepo.groovy
@@ -11,12 +11,12 @@ def repoName = '"'+resultJson.name+'"'
   customHeaders: [[maskValue: false, name: 'Content-Type', value: 'application/json'], 
                     [maskValue: false, name: 'Accept', value: 'application/json']], 
  httpMode: 'POST', requestBody: """{
-{
+
     "name": ${repoName},
     "scmId": "git",
     "forkable": true
 }""", responseHandle: 'NONE', url:'https://api.bitbucket.org/2.0/repositories/Megalai/Proj'
-}
+
 
 
 def call(){
