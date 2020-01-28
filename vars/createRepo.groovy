65c3bbbff4a8af514924280920bab77a13075ad8
REVATHISARAISWATHI <36625520+REVATHISARAISWATHI@users.noreply.github.com>
2020-01-28 22:24:42 +0530

    Update createRepo.groovy

--- vars/createRepo.groovy
+++ vars/createRepo.groovy
@@ -7,11 +7,7 @@ def resultJson = jsonSlurper.parseText(data)
 def repoName = '"'+resultJson.name+'"'
 //def projUrl = resultJson.url
   
- httpRequest authentication: 'bitbucket', 
-    customHeaders: [[maskValue: false, name: 'Content-Type', value: 'application/json'], 
-                    [maskValue: false, name: 'Accept', value: 'application/json']], 
-    httpMode: 'POST', requestBody: "name": ${repoName}, 
-    responseHandle: 'NONE', url: 'https://api.bitbucket.org/2.0/repositories/Megalai/Proj'
+ curl -X POST -v --user Megalai:admin123 -H "ContentType: application/json; charset=UTF-8" "https://api.bitbucket.org/2.0/repositories/Megalai/${repoName}" 
 }
 def call(){
 def request = libraryResource 'data.json'
