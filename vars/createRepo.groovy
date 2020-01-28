db352a2f25071dd97e7cb31387b4a6e5cac2bbd7
REVATHISARAISWATHI <36625520+REVATHISARAISWATHI@users.noreply.github.com>
2020-01-28 22:18:51 +0530

    Update createRepo.groovy

--- vars/createRepo.groovy
+++ vars/createRepo.groovy
@@ -10,11 +10,8 @@ def repoName = '"'+resultJson.name+'"'
  httpRequest authentication: 'bitbucket', 
     customHeaders: [[maskValue: false, name: 'Content-Type', value: 'application/json'], 
                     [maskValue: false, name: 'Accept', value: 'application/json']], 
-    httpMode: 'POST', requestBody: """{
-    
-    "name": ${repoName},
-    
-}""", responseHandle: 'NONE', url: 'https://api.bitbucket.org/2.0/repositories/Megalai/Proj'
+    httpMode: 'POST', requestBody: "name": ${repoName}, 
+    responseHandle: 'NONE', url: 'https://api.bitbucket.org/2.0/repositories/Megalai/Proj'
 }
 def call(){
 def request = libraryResource 'data.json'
