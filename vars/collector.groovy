a87481b7c621430b7a6325d024bcab19cef5c1b6
REVATHISARAISWATHI <36625520+REVATHISARAISWATHI@users.noreply.github.com>
2020-01-29 03:45:02 -0800

    Update collector.groovy

--- vars/collector.groovy
+++ vars/collector.groovy
@@ -5,7 +5,7 @@ collector(String data){
 def jsonSlurper = new JsonSlurper() 
 def resultJson = jsonSlurper.parseText(data)
 def projUrl = resultJson.url
-httpRequest authentication: 'bitbucket', contentType: 'APPLICATION_JSON', name: 'Content-Type', value: 'application/json', httpMode: 'GET', requestBody: """
+httpRequest authentication: 'bitbucket', contentType: 'APPLICATION_JSON', httpMode: 'GET', requestBody: """
 {
 }""", responseHandle: 'NONE', url: "${projUrl}"
 }
