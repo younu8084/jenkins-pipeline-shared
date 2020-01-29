7f49e4b229d9dd4562eb5142d476f8e9e16f36bd
REVATHISARAISWATHI <36625520+REVATHISARAISWATHI@users.noreply.github.com>
2020-01-29 10:02:27 +0530

    Update createRepo.groovy

--- vars/createRepo.groovy
+++ vars/createRepo.groovy
@@ -11,7 +11,7 @@ httpRequest authentication: 'bitbucket', contentType: 'APPLICATION_JSON', custom
     "name": ${repoName},
     "scmId": "git",
     "forkable": true
-}""", responseHandle: 'NONE', url: "https://api.bitbucket.org/2.0/repositories/Megalai/$name"
+}""", responseHandle: 'NONE', url: "https://api.bitbucket.org/2.0/repositories/Megalai/"
 }
 def call(){
 def request = libraryResource 'data.json'
