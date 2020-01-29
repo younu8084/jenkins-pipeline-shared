667f4bccf8fbb71392d30ac35ba28a1cff040b8e
REVATHISARAISWATHI <36625520+REVATHISARAISWATHI@users.noreply.github.com>
2020-01-29 01:39:10 -0800

    Update bitbucketcollector.groovy

--- vars/bitbucketcollector.groovy
+++ vars/bitbucketcollector.groovy
@@ -1,13 +1,4 @@
 import groovy.json.JsonSlurper
-String apiurl1 = "https://api.bitbucket.org/2.0/repositories/Megalai"
-String username = "Megalai" 
-String password = "admin123"
-String userpass = username + ":" + password; 
-String basicAuth = "Basic " + javax.xml.bind.DatatypeConverter.printBase64Binary(userpass.getBytes());
+def json = new JsonSlurper().parseText( new URL( 'https://api.bitbucket.org/2.0/repositories/Megalai/namerepo' ).text )
 
-URL url = apiurl1.toURL();
-URLConnection connection = url.openConnection();
-connection.setRequestProperty("Authorization",basicAuth);
-InputStream inputStream = connection.getInputStream();
-def names = new groovy.json.JsonSlurper().parseText(inputStream.text);
-connection.disconnect();
+json.each { println it }
