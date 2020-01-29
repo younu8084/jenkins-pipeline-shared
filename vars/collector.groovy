bdb97ffa4306885e43a1fc2d789286199e7df70c
REVATHISARAISWATHI <36625520+REVATHISARAISWATHI@users.noreply.github.com>
2020-01-29 02:35:34 -0800

    Update collector.groovy

--- vars/collector.groovy
+++ vars/collector.groovy
@@ -5,7 +5,6 @@ collector(String data){
 def jsonSlurper = new JsonSlurper() 
 def resultJson = jsonSlurper.parseText(data)
 def projUrl = resultJson.url
-  sh 'curl -v -G "${projUrl}" -d{"url":"${projUrl}"}'
 }
 def call(){
 def response = libraryResource 'datacollector.json'
