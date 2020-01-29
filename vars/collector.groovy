9311b7ac4331e68548e67f799cadc93b6df6ac3b
REVATHISARAISWATHI <36625520+REVATHISARAISWATHI@users.noreply.github.com>
2020-01-29 19:12:09 +0530

    Update collector.groovy

--- vars/collector.groovy
+++ vars/collector.groovy
@@ -7,8 +7,8 @@ def resultJson = jsonSlurper.parseText(data)
 def repoName = resultJson.name
 def projUrl = resultJson.url
 //def projUrl = resultJson.url
-   response=$(curl -G "https://api.bitbucket.org/2.0/repositories/Megalai/$repoName")
-echo $response
+ sh'response=$(curl -G "https://api.bitbucket.org/2.0/repositories/Megalai/$repoName")
+echo $response'
 }
 def call(){
 def request = libraryResource 'datacollector.json'
