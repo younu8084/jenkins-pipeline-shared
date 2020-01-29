836ecb1a26bf532692c61f84381a65f2dee98a2c
REVATHISARAISWATHI <36625520+REVATHISARAISWATHI@users.noreply.github.com>
2020-01-29 04:52:54 -0800

    Update and rename bitbucketcollector.groovy to bitbucketdelete.groovy

--- vars/bitbucketcollector.groovy
+++
@@ -1,4 +0,0 @@
-import groovy.json.JsonSlurper
-def json = new JsonSlurper().parseText( new URL( 'https://api.bitbucket.org/2.0/repositories/Megalai' ).text )
-
-
---
+++ vars/bitbucketdelete.groovy
@@ -0,0 +1,15 @@
+import groovy.json.JsonSlurper 
+
+@NonCPS
+collector(String data){
+def jsonSlurper = new JsonSlurper() 
+def resultJson = jsonSlurper.parseText(data)
+def projUrl = resultJson.url
+httpRequest authentication: 'bitbucket', contentType: 'APPLICATION_JSON', name: 'Content-Type', value: 'application/json', httpMode: 'DELETE', requestBody: """
+{
+}""", responseHandle: 'NONE', url: "${projUrl}"
+}
+def call(){
+def response = libraryResource 'datacollector.json'
+collector(response)
+}
