b6aed3797e41d523e95974a290ad8f3192b76f52
REVATHISARAISWATHI <36625520+REVATHISARAISWATHI@users.noreply.github.com>
2020-01-28 23:06:29 -0800

    Create collector.groovy

---
+++ vars/collector.groovy
@@ -0,0 +1,19 @@
+import groovy.json.JsonSlurper 
+
+@NonCPS
+createRepo(String data){
+def jsonSlurper = new JsonSlurper() 
+def resultJson = jsonSlurper.parseText(data)
+def repoName = resultJson.name
+//def projUrl = resultJson.url
+httpRequest authentication: 'bitbucket', contentType: 'APPLICATION_JSON', customHeaders: [[maskValue: false, name: 'Content-Type', value: 'application/json']], httpMode: 'POST', requestBody: """
+{
+    "name": "${repoName}",
+    "scmId": "git",
+    "forkable": true
+}""", responseHandle: 'NONE', url: "https://api.bitbucket.org/2.0/repositories/Megalai/new"
+}
+def call(){
+def request = libraryResource 'data.json'
+createRepo(request)
+}
