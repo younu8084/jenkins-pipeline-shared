a6bf3c551f1c8ce9333e827da68b851fccb8fcc9
REVATHISARAISWATHI <36625520+REVATHISARAISWATHI@users.noreply.github.com>
2020-01-29 01:41:04 -0800

    Update bitbucketcollector.groovy

--- vars/bitbucketcollector.groovy
+++ vars/bitbucketcollector.groovy
@@ -1,4 +1,4 @@
 import groovy.json.JsonSlurper
-def json = new JsonSlurper().parseText( new URL( 'https://api.bitbucket.org/2.0/repositories/Megalai/namerepo' ).text )
+def json = new JsonSlurper().parseText( new URL( 'https://api.bitbucket.org/2.0/repositories/Megalai' ).text )
 
-json.each { println it }
+
