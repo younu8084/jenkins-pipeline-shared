e867d2aaad599d5ff47bdd6beed5bf780987bec0
REVATHISARAISWATHI <36625520+REVATHISARAISWATHI@users.noreply.github.com>
2020-01-29 04:48:25 -0800

    Update collector.groovy

--- vars/collector.groovy
+++ vars/collector.groovy
@@ -1,13 +1,4 @@
-import groovy.json.JsonSlurper 
-
-@NonCPS
-collector(String data){
-def jsonSlurper = new JsonSlurper() 
-def resultJson = jsonSlurper.parseText(data)
-def projUrl = resultJson.url
+def call(){
   sh 'curl -v -G  https://api.bitbucket.org/2.0/repositories/Megalai '
 }
-def call(){
-def response = libraryResource 'datacollector.json'
-collector(response)
-}
+
