1ce79276341505bc5cd8d36ce188e7f9b8d91bfa
REVATHISARAISWATHI <36625520+REVATHISARAISWATHI@users.noreply.github.com>
2020-01-30 17:56:17 +0530

    Delete createRepo.groovy

--- vars/createRepo.groovy
+++
@@ -1,9 +0,0 @@
-def call()
-{
-//You can change the log content
-def content = "This is the message that user like to log, but to a file"
-//You can change the file name
-def fileName = '/tmp/test.log'
-//Write to the file
-new File(fileName).write(content)
-}
