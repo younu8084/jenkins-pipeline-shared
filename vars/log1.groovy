0a9801c95125d095fb38e14056fe13935ddcba5c
REVATHISARAISWATHI <36625520+REVATHISARAISWATHI@users.noreply.github.com>
2020-01-30 06:01:55 -0800

    Update log1.groovy

--- vars/log1.groovy
+++ vars/log1.groovy
@@ -1,10 +1,5 @@
-import io.wcm.devops.jenkins.pipeline.utils.logging.LogLevel
-import io.wcm.devops.jenkins.pipeline.utils.logging.Logger
+
 def call()
 { 
-Logger.init(this, [ logLevel: LogLevel.TRACE ])
-Logger log = new Logger(this)
-
-log.trace("I am a trace log message")
-
+package log4jclass HelloWorldLog {   static void main(args) {      def name = "World"      Log.info "Hello $name!"
 }
