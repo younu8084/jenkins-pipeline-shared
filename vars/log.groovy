c0c51caf5ead0e95c9c2056e4e3da1ca4f16b62e
REVATHISARAISWATHI <36625520+REVATHISARAISWATHI@users.noreply.github.com>
2020-01-30 05:40:11 -0800

    Update log.groovy

--- vars/log.groovy
+++ vars/log.groovy
@@ -1,13 +1,14 @@
 #!/bin/bash
- 
+def call(){ 
 log=log_file.txt
  
-# create log file or overrite if already present
+
 printf "Log File - " > $log
  
-# append date to log file
+
 date >> $log
  
 x=$(( 3 + 1 ))
-# append some data to log file
+
 echo value of x is $x >> $log
+}
