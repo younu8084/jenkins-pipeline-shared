6d43501990c433101f6fe4c619a1b4d2290e39c5
REVATHISARAISWATHI <36625520+REVATHISARAISWATHI@users.noreply.github.com>
2020-01-30 11:40:57 +0530

    Delete bitbucketcollector.groovy

--- bitbucketcollector.groovy
+++
@@ -1,7 +0,0 @@
-def connection = new URL( "https://api.bitbucket.org/2.0/repositories/Megalai")
-        .openConnection() as HttpURLConnection
-
-connection.setRequestProperty( 'Accept', 'application/json' )
-
-// get the response code - automatically sends the request
-println connection.inputStream.text
