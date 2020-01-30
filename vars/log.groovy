a355e2271c313db4cb503e0fea05d6c273cd7518
REVATHISARAISWATHI <36625520+REVATHISARAISWATHI@users.noreply.github.com>
2020-01-30 05:28:41 -0800

    Update log.groovy

--- vars/log.groovy
+++ vars/log.groovy
@@ -1,23 +1,10 @@
 def call()
 {
-filename = 'examples/data/count_digits.txt'
+
+File file = new File("out.txt")
+file.write "First line\n"
+file << "Second line\n"
  
-// read all the content of the file into a single string
-File fh1 = new File(filename)
-text = fh1.getText('UTF-8')
- 
-// read all the lines into a list, each line is an element in the list
-File fh2 = new File(filename)
-def lines = fh2.readLines()
-for (line in lines) {
-    // ...
-}
- 
- 
-File fh3 = new File(filename)
-LineNumberReader reader = fh3.newReader()
-while ((line = reader.readLine()) != null) {
-    // ...
-}
+println file.text
 
 }
