e136ea667ac8eea932f5da42cd0a27facfc7e1d2
REVATHISARAISWATHI <36625520+REVATHISARAISWATHI@users.noreply.github.com>
2020-01-30 05:35:28 -0800

    Update log.groovy

--- vars/log.groovy
+++ vars/log.groovy
@@ -4,13 +4,11 @@ def call()
 LOGFILE=/home/test/test.log
 echo "`date +%H:%M:%S : Starting work" >> $LOGFILE
 
-service test stop >> $LOGFILE
+
 
 mv /var/run/test/test.pid /var/run/test/test.pid.bak
 
 echo "`date +%H:%M:%S : process file moved" >> $LOGFILE
-
-service test start >> $LOGFILE
 
 echo "`date +%H:%M:%S : Finished" >> $LOGFILE
 }
