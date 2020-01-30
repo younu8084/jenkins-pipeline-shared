90a2092a7b5a893acccddeead35189974abb78bd
REVATHISARAISWATHI <36625520+REVATHISARAISWATHI@users.noreply.github.com>
2020-01-30 03:56:05 -0800

    Create createRepo.groovy

---
+++ vars/createRepo.groovy
@@ -0,0 +1,6 @@
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+final Logger logger = LoggerFactory.getLogger("myGroovyLogger");
+logger.trace("Test log output message");
+myGroovyLogger.level=FINEST
