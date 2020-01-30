185af8267ee15a28df535a7d36c3f9d8e3b153f9
REVATHISARAISWATHI <36625520+REVATHISARAISWATHI@users.noreply.github.com>
2020-01-30 06:03:45 -0800

    Update log1.groovy

--- vars/log1.groovy
+++ vars/log1.groovy
@@ -1,5 +1,30 @@
+import java.util.logging.Logger
+import java.util.logging.FileHandler
+import java.util.logging.SimpleFormatter
+import java.io.IOException
+import java.util.logging.LogManager
+import java.util.logging.Level
 
-def call()
-{ 
-package log4jclass HelloWorldLog {   static void main(args) {      def name = "World"      Log.info "Hello $name!"
+int FILE_SIZE = 1024
+
+
+def logger = Logger.getLogger("")
+LogManager.getLogManager().reset()
+
+
+try {
+	//
+	// Creating an instance of FileHandler with 5 logging files
+	// sequences.
+	//
+	FileHandler handler = new FileHandler("myapp.log", FILE_SIZE, 5, true);
+	handler.setFormatter(new SimpleFormatter());
+	logger.addHandler(handler);
+	//logger.setUseParentHandlers(false);
+} catch (IOException e) {
+	logger.warning("Failed to initialize logger handler.");
+}
+
+for (i = 0; i < 5000; i++) {
+	logger.info ("I am a test info log")
 }
