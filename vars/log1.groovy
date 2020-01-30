47d32ddf540cdd6596544d0c3581a3dd9548fd7b
REVATHISARAISWATHI <36625520+REVATHISARAISWATHI@users.noreply.github.com>
2020-01-30 06:05:05 -0800

    Update log1.groovy

--- vars/log1.groovy
+++ vars/log1.groovy
@@ -6,7 +6,7 @@ import java.util.logging.LogManager
 import java.util.logging.Level
 
 int FILE_SIZE = 1024
-
+def call(){
 
 def logger = Logger.getLogger("")
 LogManager.getLogManager().reset()
@@ -27,4 +27,5 @@ try {
 
 for (i = 0; i < 5000; i++) {
 	logger.info ("I am a test info log")
+}
 }
