3787fc17a22ea49d7668cfd8acc289a109186519
REVATHISARAISWATHI <36625520+REVATHISARAISWATHI@users.noreply.github.com>
2020-01-30 04:00:53 -0800

    Update createRepo.groovy

--- vars/createRepo.groovy
+++ vars/createRepo.groovy
@@ -1,6 +1,6 @@
-def call(){
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
+def call(){
 
 final Logger logger = LoggerFactory.getLogger("myGroovyLogger");
 logger.trace("Test log output message");
