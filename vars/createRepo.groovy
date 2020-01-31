396cc06026accaba4c7e326c71ed85ec60a615a4
REVATHISARAISWATHI <36625520+REVATHISARAISWATHI@users.noreply.github.com>
2020-01-31 12:24:53 +0530

    Update createRepo.groovy

--- vars/createRepo.groovy
+++ vars/createRepo.groovy
@@ -5,7 +5,7 @@ createRepo(String data){
 def jsonSlurper = new JsonSlurper() 
 def resultJson = jsonSlurper.parseText(data)
 def repoName = resultJson.bitbucketname
-def projUrl = resultJson.url
+def projUrl = resultJson.bitbucketurl
 //def projUrl = resultJson.url
 httpRequest authentication: 'bitbucket', contentType: 'APPLICATION_JSON', customHeaders: [[maskValue: false, name: 'Content-Type', value: 'application/json']], httpMode: 'POST', requestBody: """
 {
