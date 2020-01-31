e2d389ac5f19ff2fce19409219f4d0821f8ce9e9
REVATHISARAISWATHI <36625520+REVATHISARAISWATHI@users.noreply.github.com>
2020-01-31 11:55:59 +0530

    Update logfunction.groovy

--- vars/logfunction.groovy
+++ vars/logfunction.groovy
@@ -8,5 +8,5 @@ def call(message)
  def resultJson = jsonSlurper.parseText(request)
  def repoName = resultJson.name
   Date date = new Date() 
-  sh " echo '${date}' '${repoName}' ${message} in Bitbucket>>log.txt"
+  sh " echo '${date}' '${repoName}' repository ${message} in Bitbucket>>log.txt"
 }
