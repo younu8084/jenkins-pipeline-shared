bf67ef6d6432c9a85f9e563121a38e3aca44635e
REVATHISARAISWATHI <36625520+REVATHISARAISWATHI@users.noreply.github.com>
2020-01-31 11:45:25 +0530

    Create logfunction.groovy

---
+++ vars/logfunction.groovy
@@ -0,0 +1,10 @@
+def call(message)
+{
+ println(message)
+ def request = libraryResource 'data.json'
+ def jsonSlurper = new JsonSlurper() 
+ def resultJson = jsonSlurper.parseText(request)
+ def projectName = resultJson.key
+  Date date = new Date() 
+  sh " echo '${date}' JIRA project with the projectname '${projectName}' ${message} >>log.txt"
+}
