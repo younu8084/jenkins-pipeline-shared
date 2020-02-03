04a37e265b70a84d8a050907daa93d875a0e7ca3
REVATHISARAISWATHI <36625520+REVATHISARAISWATHI@users.noreply.github.com>
2020-02-03 18:18:57 +0530

    Update listbucketcollector.groovy

--- vars/listbucketcollector.groovy
+++ vars/listbucketcollector.groovy
@@ -1,5 +1,6 @@
 
-def call(){
-sh 'curl -X -GET --user rig:rigaDapt@devOps "http://18.224.68.30:7990/rest/api/1.0/projects/BIT/repos"'
-
+def call()
+{
+sh 'curl -v -G --user rig:rigaDapt@devOps http://18.224.68.30:7990/rest/api/1.0/projects/BIT/repos'
+'
 }
