79ec734d06da4610957dd38e0543e72722632e04
REVATHISARAISWATHI <36625520+REVATHISARAISWATHI@users.noreply.github.com>
2020-02-03 18:10:45 +0530

    Update listbucketcollector.groovy

--- vars/listbucketcollector.groovy
+++ vars/listbucketcollector.groovy
@@ -1,5 +1,5 @@
 import groovy.json.JsonSlurper 
 def call(){
-sh 'curl -X -GET --user rig:rigaDapt@devOps  "http://18.224.68.30:7990//rest/api/1.0/projects/BIT/repos"'
+sh 'curl -X -GET --user rig:rigaDapt@devOps  "http://18.224.68.30:7990/rest/api/1.0/projects/BIT/repos"'
 
 }
