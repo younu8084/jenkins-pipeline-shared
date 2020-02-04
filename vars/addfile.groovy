fd58a84a426502b168d3ee0a7e5b66030f659257
REVATHISARAISWATHI <36625520+REVATHISARAISWATHI@users.noreply.github.com>
2020-02-04 01:10:12 -0800

    Update addfile.groovy

--- vars/addfile.groovy
+++ vars/addfile.groovy
@@ -1,3 +1,3 @@
 def call(){
-sh 'curl -u rig:rigaDapt@devOps -X POST http://18.224.68.30:7990//rest/api/1.0/projects/BIT/newrepo -F filename.ext=@filename.ext
+sh 'curl -u rig:rigaDapt@devOps -X POST http://18.224.68.30:7990//rest/api/1.0/projects/BIT/newrepo -F filename.ext=@filename.ext'
 }
