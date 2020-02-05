1366b6575d4167dadde74ba717afd3b180f3aa17
REVATHISARAISWATHI <36625520+REVATHISARAISWATHI@users.noreply.github.com>
2020-02-05 11:55:24 +0530

    Update createRepo1.groovy

--- vars/createRepo1.groovy
+++ vars/createRepo1.groovy
@@ -13,7 +13,7 @@ env.key = projectKey
 
 httpRequest authentication: 'bitbucket_cred', contentType: 'APPLICATION_JSON', customHeaders: [[maskValue: false, name: 'Content-Type', value: 'application/json']], httpMode: 'POST', requestBody: """
 {
-    "key": "${projectKey}"
+    "key": "${projectKey}",
     "name": "${projectName}",
      public: true
 }""", responseHandle: 'NONE', url:"http://18.224.68.30:7990/rest/api/1.0/projects"
