231e5efd3a1a971c5b8140b1659dee0fa1b404a6
REVATHISARAISWATHI <36625520+REVATHISARAISWATHI@users.noreply.github.com>
2020-02-05 04:23:29 -0800

    Update BBbranchcollector.groovy

--- vars/BBbranchcollector.groovy
+++ vars/BBbranchcollector.groovy
@@ -1,4 +1,4 @@
 def call()
 {
-sh 'curl -v -G --user rig:rigaDapt@devOps "http://18.224.68.30:7990/rest/api/1.0/projects/BIT/repos/newrepo/branches"'
+sh 'curl -v -G --user rig:rigaDapt@devOps http://18.224.68.30:7990/rest/api/1.0/projects/BIT/repos/newrepo/branches'
 }
