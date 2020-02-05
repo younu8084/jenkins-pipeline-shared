0ababb55d3dbb766f8294f68f1ec38e14adb25b1
REVATHISARAISWATHI <36625520+REVATHISARAISWATHI@users.noreply.github.com>
2020-02-05 12:45:13 +0530

    Create Bbrepo.groovy

---
+++ vars/Bbrepo.groovy
@@ -0,0 +1,19 @@
+def call(jsondata){
+def jsonString = jsondata
+//println(jsonString)
+def jsonObj = readJSON text: jsonString
+println(jsonObj.scm)
+
+String a=jsonObj.scm.projects.repository.repo_name
+String repoName=a.replaceAll("\\[", "").replaceAll("\\]","");
+String b=jsonObj.scm.projects.project.project_key 
+String projectKey=b.replaceAll("\\[", "").replaceAll("\\]","");
+
+httpRequest authentication: 'bitbucket_cred', contentType: 'APPLICATION_JSON', customHeaders: [[maskValue: false, name: 'Content-Type', value: 'application/json']], httpMode: 'POST', requestBody: """
+{
+    "name": "${repoName}",
+    "scmId": "git",
+    "forkable": true
+}""", responseHandle: 'NONE', url:"http://18.224.68.30:7990/rest/api/1.0/projects/${projkey}/repos"
+  
+  }
