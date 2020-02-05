b75ea5d3726fbe44c7b68beb09d70d3dc3a913c0
REVATHISARAISWATHI <36625520+REVATHISARAISWATHI@users.noreply.github.com>
2020-02-05 13:21:06 +0530

    Update Bbbranch.groovy

--- vars/Bbbranch.groovy
+++ vars/Bbbranch.groovy
@@ -14,7 +14,7 @@ String d=jsonObj.scm.projects.project.repositories.repository.branches.branch.na
 String branch=d.replaceAll("\\[", "").replaceAll("\\]","");
 httpRequest authentication: 'bitbucket_cred', contentType: 'APPLICATION_JSON', customHeaders: [[maskValue: false, name: 'Content-Type', value: 'application/json']], httpMode: 'POST', requestBody: """
 {
-    "name": "$branch",
+    "name": "${branch}",
     "startPoint": "${sPoint}"   
 }""", responseHandle: 'NONE', url:"http://18.224.68.30:7990/rest/api/1.0/projects/${projectKey}/repos/${repoName}/branches"
   
