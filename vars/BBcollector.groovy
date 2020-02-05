27657efa7d89e069a80ae1b465f679e906d96fa9
REVATHISARAISWATHI <36625520+REVATHISARAISWATHI@users.noreply.github.com>
2020-02-05 01:49:09 -0800

    Create BBcollector.groovy

---
+++ vars/BBcollector.groovy
@@ -0,0 +1,23 @@
+def call(jsondata){
+def jsonString = jsondata
+//println(jsonString)
+def jsonObj = readJSON text: jsonString
+println(jsonObj.scm)
+
+String a=jsonObj.scm.projects.project.project_name
+String projectName=a.replaceAll("\\[", "").replaceAll("\\]","");
+String b=jsonObj.scm.projects.project.project_key 
+String projectKey=b.replaceAll("\\[", "").replaceAll("\\]","");
+String c=jsonObj.scm.projects.project.project_description 
+String projectdes=c.replaceAll("\\[", "").replaceAll("\\]","");
+
+
+httpRequest authentication: 'bitbucket_cred', contentType: 'APPLICATION_JSON', customHeaders: [[maskValue: false, name: 'Content-Type', value: 'application/json']], httpMode: 'GET', requestBody: """
+{
+    "key": "${projectKey}",
+    "name": "${projectName}",
+    "description": "${projectdes}"
+    
+}""", responseHandle: 'NONE', url:"http://18.224.68.30:7990/rest/api/1.0/projects"
+  
+  }
