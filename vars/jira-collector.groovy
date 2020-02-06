3139aa0ee5b405a7ecb991f838406062bf764b9f
REVATHISARAISWATHI <36625520+REVATHISARAISWATHI@users.noreply.github.com>
2020-02-06 15:36:10 +0530

    Update jira-collector.groovy

--- vars/jira-collector.groovy
+++ vars/jira-collector.groovy
@@ -4,10 +4,6 @@ def jsonObj = readJSON text: jsonString
 
 String b=jsonObj.alm.projects.project.project_typeKey
 String Key=b.replaceAll("\\[", "").replaceAll("\\]","");
-String c=jsonObj.alm.projects.project.project_key 
-String Key=c.replaceAll("\\[", "").replaceAll("\\]","");
-String d=jsonObj.alm.projects.project.project_key
-String Key=d.replaceAll("\\[", "").replaceAll("\\]","");
 println(Key)
 sh "curl -X GET -i -H  -d  -u rig:digitalrig@123 http://ec2-18-191-16-16.us-east-2.compute.amazonaws.com:8080/rest/api/2/search?jql=project%3D${Key}&fields=key%2Csummary%2Cdescription&maxResults=1000&startAt=0 "
 //sh "curl -X GET -i -H  -d  -u rig:digitalrig@123 http://ec2-18-191-16-16.us-east-2.compute.amazonaws.com:8080 /rest/api/2/issue/${issueIdOrKey} " 
