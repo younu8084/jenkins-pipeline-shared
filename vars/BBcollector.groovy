ebb1d92aa435c77e6ec80ad4e00ab85131af82fe
REVATHISARAISWATHI <36625520+REVATHISARAISWATHI@users.noreply.github.com>
2020-02-06 06:34:12 -0800

    Update BBcollector.groovy

--- vars/BBcollector.groovy
+++ vars/BBcollector.groovy
@@ -6,10 +6,10 @@ String b=jsonObj.scm.projects.project.project_key
 String Key=b.replaceAll("\\[", "").replaceAll("\\]","");
 println(Key)
  withCredentials([usernamePassword(credentialsId: 'bitbucket_cred', passwordVariable: 'pass', usernameVariable: 'userId')]) {
-sh "curl -X GET -i -H -u $userId:$pass http://18.224.68.30:7990/rest/api/1.0/projects/'${Key}'/repos/Rig" 
-sh "curl -X GET -i -H -u $userId:$pass http://18.224.68.30:7990/rest/api/1.0/projects/'${Key}'"
-sh "curl -X GET -i -H -u $userId:$pass http://18.224.68.30:7990/rest/api/1.0/projects/'${Key}'/repos/Rig/branches" 
-sh 'curl -X GET -i -H -u $userId:$pass "http://18.224.68.30:7990/rest/api/1.0/admin/groups"'
- sh "curl -X GET -i -H -u $userId:$pass http://18.224.68.30:7990/rest/api/1.0/projects/'${Key}'/repos/rig/commits" 
+sh "curl GET  -H -u $userId:$pass http://18.224.68.30:7990/rest/api/1.0/projects/'${Key}'/repos/Rig" 
+sh "curl GET  -H -u $userId:$pass http://18.224.68.30:7990/rest/api/1.0/projects/'${Key}'"
+sh "curl GET  -H -u $userId:$pass http://18.224.68.30:7990/rest/api/1.0/projects/'${Key}'/repos/Rig/branches" 
+sh 'curl GET -H -u $userId:$pass "http://18.224.68.30:7990/rest/api/1.0/admin/groups"'
+ sh "curl GET -H -u $userId:$pass http://18.224.68.30:7990/rest/api/1.0/projects/'${Key}'/repos/rig/commits" 
  }
 }
