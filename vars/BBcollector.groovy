edc437ad4e04a885906211810a5acdc20e419acf
REVATHISARAISWATHI <36625520+REVATHISARAISWATHI@users.noreply.github.com>
2020-02-05 23:18:04 -0800

    Update BBcollector.groovy

--- vars/BBcollector.groovy
+++ vars/BBcollector.groovy
@@ -5,7 +5,7 @@ def jsonObj = readJSON text: jsonString
 String b=jsonObj.scm.projects.project.project_key 
 String Key=b.replaceAll("\\[", "").replaceAll("\\]","");
 println(Key)
-sh "curl -X GET -i -H  -d  -u rig:rigaDapt@devOps http://18.224.68.30:7990/rest/api/1.0/projects/'${Key}'/repos" 
+sh "curl -X GET -i -H  -d  -u rig:rigaDapt@devOps http://18.224.68.30:7990/rest/api/1.0/projects/'${Key}'/repos/Rig" 
 sh "curl -X GET -i -H  -d  -u rig:rigaDapt@devOps http://18.224.68.30:7990/rest/api/1.0/projects/'${Key}'"
 sh "curl -X GET -i -H  -d  -u rig:rigaDapt@devOps http://18.224.68.30:7990/rest/api/1.0/projects/'${Key}'/repos/Rig/branches" 
 sh "curl -X GET -i -H  -d  -u rig:rigaDapt@devOps  http://18.224.68.30:7990/rest/api/1.0/projects/'${Key}'/repos/Rig/tags " 
