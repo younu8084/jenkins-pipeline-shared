3d34e015122e1a3e9d05b82e648dc2242fd889fa
REVATHISARAISWATHI <36625520+REVATHISARAISWATHI@users.noreply.github.com>
2020-02-05 23:15:03 -0800

    Update BBcollector.groovy

--- vars/BBcollector.groovy
+++ vars/BBcollector.groovy
@@ -5,10 +5,10 @@ def jsonObj = readJSON text: jsonString
 String b=jsonObj.scm.projects.project.project_key 
 String Key=b.replaceAll("\\[", "").replaceAll("\\]","");
 println(Key)
-sh 'curl -X GET -i -H  -d  -u rig:rigaDapt@devOps "http://18.224.68.30:7990/rest/api/1.0/projects/'${Key}'/repos" '
+sh "curl -X GET -i -H  -d  -u rig:rigaDapt@devOps http://18.224.68.30:7990/rest/api/1.0/projects/'${Key}'/repos" 
 sh "curl -X GET -i -H  -d  -u rig:rigaDapt@devOps http://18.224.68.30:7990/rest/api/1.0/projects/'${Key}'"
-sh 'curl -X GET -i -H  -d  -u rig:rigaDapt@devOps "http://18.224.68.30:7990/rest/api/1.0/projects/'${Key}'/repos/Rig/branches" '
+sh "curl -X GET -i -H  -d  -u rig:rigaDapt@devOps http://18.224.68.30:7990/rest/api/1.0/projects/'${Key}'/repos/Rig/branches" 
 sh "curl -X GET -i -H  -d  -u rig:rigaDapt@devOps  http://18.224.68.30:7990/rest/api/1.0/projects/'${Key}'/repos/Rig/tags " 
- sh 'curl -X GET -i -H  -d  -u rig:rigaDapt@devOps "http://18.224.68.30:7990/rest/api/1.0/projects/'${Key}'/repos/rig/commits" '
+ sh "curl -X GET -i -H  -d  -u rig:rigaDapt@devOps http://18.224.68.30:7990/rest/api/1.0/projects/'${Key}'/repos/rig/commits" 
 
 }
