4493fd7c77c91c379711c275d5488f0ea9a0e815
REVATHISARAISWATHI <36625520+REVATHISARAISWATHI@users.noreply.github.com>
2020-02-05 23:05:11 -0800

    Update BBcollector.groovy

--- vars/BBcollector.groovy
+++ vars/BBcollector.groovy
@@ -8,7 +8,7 @@ println(Key)
 sh 'curl -X GET -i -H  -d  -u rig:rigaDapt@devOps "http://18.224.68.30:7990/rest/api/1.0/projects/EDN/repos" '
 sh "curl -X GET -i -H  -d  -u rig:rigaDapt@devOps http://18.224.68.30:7990/rest/api/1.0/projects/'${Key}'"
 sh 'curl -X GET -i -H  -d  -u rig:rigaDapt@devOps "http://18.224.68.30:7990/rest/api/1.0/projects/EDN/repos/Rig/branches" '
-sh "curl -X GET -i -H  -d  -u rig:rigaDapt@devOps http://18.224.68.30:7990/rest/api/1.0/projects/'${Key}'/Rig/tags " 
+sh "curl -X GET -i -H  -d  -u rig:rigaDapt@devOps  http://18.224.68.30:7990/rest/api/1.0/projects/'${Key}'/repos/Rig/tags " 
  sh 'curl -X GET -i -H  -d  -u rig:rigaDapt@devOps "http://18.224.68.30:7990/rest/api/1.0/users/rig/repos/Rig/commits" '
 
 }
