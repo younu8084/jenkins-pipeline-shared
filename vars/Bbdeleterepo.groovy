845440e73ec89de463a7af42f246f337676bd2fb
REVATHISARAISWATHI <36625520+REVATHISARAISWATHI@users.noreply.github.com>
2020-02-06 19:17:32 +0530

    Create Bbdeleterepo.groovy

---
+++ vars/Bbdeleterepo.groovy
@@ -0,0 +1,17 @@
+def call(jsondata){
+def jsonString = jsondata
+//println(jsonString)
+def jsonObj = readJSON text: jsonString
+println(jsonObj.scm)
+
+String a=jsonObj.scm.projects.project.repositories[0].repository[1].repo_name
+String repoName=a.replaceAll("\\[", "").replaceAll("\\]","");
+String b=jsonObj.scm.projects.project.project_key 
+String projectKey=b.replaceAll("\\[", "").replaceAll("\\]","");
+
+httpRequest authentication: 'bitbucket_cred', contentType: 'APPLICATION_JSON', customHeaders: [[maskValue: false, name: 'Content-Type', value: 'application/json']], httpMode: 'DELETE', requestBody: """
+{
+    
+}""", responseHandle: 'NONE', url:"http://18.224.68.30:7990/rest/api/1.0/projects/${projectKey}/repos/${repoName}"
+  
+  }
