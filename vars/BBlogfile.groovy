9d201dc5f4456b3ed96d4679388c8b0d2dd78e4b
REVATHISARAISWATHI <36625520+REVATHISARAISWATHI@users.noreply.github.com>
2020-02-06 23:10:21 -0800

    Update BBlogfile.groovy

--- vars/BBlogfile.groovy
+++ vars/BBlogfile.groovy
@@ -1,5 +1,13 @@
-def call(message)
+def call(message,JSON)
 {
+  
+def jsonString = JSON
+def jsonObj = readJSON text: jsonString
+println(jsonObj.scm)
+
+String b=jsonObj.scm.projects.project.project_key 
+String Key=b.replaceAll("\\[", "").replaceAll("\\]","");
+
  println(message)
   Date date = new Date() 
  
