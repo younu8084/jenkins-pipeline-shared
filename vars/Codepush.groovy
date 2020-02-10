f2c1224d8c1d98eccf157a4d04b7f29ecafc8d57
REVATHISARAISWATHI <36625520+REVATHISARAISWATHI@users.noreply.github.com>
2020-02-10 17:25:42 +0530

    Update Codepush.groovy

--- vars/Codepush.groovy
+++ vars/Codepush.groovy
@@ -9,7 +9,7 @@ String repoName=a.replaceAll("\\[", "").replaceAll("\\]","");
 String b=jsonObj.scm.projects.project.project_key 
 String projectKey=b.replaceAll("\\[", "").replaceAll("\\]","");
   
-  def link1 = " find . ! -name log.txt -delete && git clone https://github.com/kavi1997/TestProject.git && git clone http://rig:rigaDapt%40devOps@18.224.68.30:7990/scm/'${projectKey}'/'${repoName}'.git && cp -r ./TestProject/* ./app && pwd && cd app && pwd && git init && git add --all && git status && git commit -m 'commit' && git remote add origin http://rig:rigaDapt%40devOps@18.224.68.30:7990/scm/'${projectKey}'/'${repoName}'.git && git push -u origin master"
+  def link1 = " find . ! -name log_'${BUILD_NUMBER}'.txt -delete && git clone https://github.com/kavi1997/TestProject.git && git clone http://rig:rigaDapt%40devOps@18.224.68.30:7990/scm/'${projectKey}'/'${repoName}'.git && cp -r ./TestProject/* ./app && pwd && cd app && pwd && git init && git add --all && git status && git commit -m 'commit' && git remote add origin http://rig:rigaDapt%40devOps@18.224.68.30:7990/scm/'${projectKey}'/'${repoName}'.git && git push -u origin master"
    
   
    sh(link1)
