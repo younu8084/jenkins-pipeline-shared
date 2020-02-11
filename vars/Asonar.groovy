240f2568d6d38b57ffb66ca727a66dc89e7cb469
REVATHISARAISWATHI <36625520+REVATHISARAISWATHI@users.noreply.github.com>
2020-02-11 19:53:50 +0530

    Update Asonar.groovy

--- vars/Asonar.groovy
+++ vars/Asonar.groovy
@@ -1,4 +1,11 @@
-def call(){
-  
-sh 'curl -X GET -u admin:admin http://3.16.33.107:9000/api/measures/component?component=comrades.bmi%3ABMI&metricKeys=vulnerabilities '
+def call(jsondata){
+def jsonString = jsondata
+//println(jsonString)
+def jsonObj = readJSON text: jsonString
+//println(jsonObj.code_quality)
+
+
+
+	sh "curl --location --request GET 'http://3.16.33.107:9000/api/measures/component?metricKeys=vulnerabilities&component=comrades.bmi%3ABMI' \
+--header 'Authorization: Basic YWRtaW46YWRtaW4='"
 }
