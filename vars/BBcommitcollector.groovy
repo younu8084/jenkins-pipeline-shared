20941f7b8c0f1e5e84685a997d8bccab671edef3
REVATHISARAISWATHI <36625520+REVATHISARAISWATHI@users.noreply.github.com>
2020-02-11 19:52:50 +0530

    Update BBcommitcollector.groovy

--- vars/BBcommitcollector.groovy
+++ vars/BBcommitcollector.groovy
@@ -1,5 +1,5 @@
 def call()
 {
-def json= sh 'curl -v -G --user rig:rigaDapt@devOps http://18.224.68.30:7990/rest/api/1.0/projects/edn/repos/rig/commits -o output.json'
+def json= sh 'curl -v -G --user rig:rigaDapt@devOps http://18.224.68.30:7990/rest/api/1.0/projects/edn/repos/rig/commits -o ouput.json'
  
 }
