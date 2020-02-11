4b0c41d11fad735b925c77091d905ebb6c89caa7
REVATHISARAISWATHI <36625520+REVATHISARAISWATHI@users.noreply.github.com>
2020-02-11 19:55:49 +0530

    Update Influxdb.groovy

--- vars/Influxdb.groovy
+++ vars/Influxdb.groovy
@@ -7,7 +7,7 @@ def reader = new BufferedReader(new InputStreamReader(new FileInputStream("/var/
 def resultJson = jsonSlurper.parse(reader)
 def size = resultJson.size
   echo "$size"
-sh """curl -i -XPOST 'http://ec2-13-58-47-71.us-east-2.compute.amazonaws.com:8086/write?db=Collector' --data-binary 'bitbucket_commits=${size}' 
+sh """curl -i -XPOST 'http://ec2-13-58-47-71.us-east-2.compute.amazonaws.com:8086/write?db=Collector' --data-binary 'bitbucketcommits=${size}' 
 """
 }
 
