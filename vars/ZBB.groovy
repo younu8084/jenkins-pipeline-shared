7e09be6c7064797d67b89f42539485734d103b6e
REVATHISARAISWATHI <36625520+REVATHISARAISWATHI@users.noreply.github.com>
2020-02-11 20:47:34 +0530

    Update ZBB.groovy

--- vars/ZBB.groovy
+++ vars/ZBB.groovy
@@ -7,6 +7,6 @@ String b=jsonObj.scm.projects.project.project_key
 String Key=b.replaceAll("\\[", "").replaceAll("\\]","");
 println(Key)
  withCredentials([usernamePassword(credentialsId: 'bitbucket_cred', passwordVariable: 'pass', usernameVariable: 'userId')]) {
-sh "curl -X GET  -H -d  -u  $userId:$pass http://18.224.68.30:7990/rest/api/1.0/projects/${Key}/repos/${repoName}/commits' -o 'output1.json'"
+sh "curl -X GET  -H -d  -u  $userId:$pass http://18.224.68.30:7990/rest/api/1.0/projects/${Key}/repos/${repoName}/commits -o output1.json"
  }
 }
