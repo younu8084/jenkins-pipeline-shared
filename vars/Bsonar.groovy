68fb77e71e930a8a9b5c09b8bceee5560a720e42
REVATHISARAISWATHI <36625520+REVATHISARAISWATHI@users.noreply.github.com>
2020-02-11 20:46:32 +0530

    Create Bsonar.groovy

---
+++ vars/Bsonar.groovy
@@ -0,0 +1,4 @@
+  
+def call(){
+def process=sh "curl --location --request GET 'http://3.16.33.107:9000/api/measures/component?metricKeys=bugs&component=comrades.bmi%3ABMI'  --header 'Authorization: Basic YWRtaW46YWRtaW4=' -o ouput1.json"
+}
