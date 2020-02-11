5947b54021e7021b0f90902f2435a7ac96254a97
REVATHISARAISWATHI <36625520+REVATHISARAISWATHI@users.noreply.github.com>
2020-02-11 19:45:29 +0530

    Update Asonar.groovy

--- vars/Asonar.groovy
+++ vars/Asonar.groovy
@@ -1,4 +1,4 @@
 def call(){
   
-sh 'curl -X GET -u admin:admin http://3.16.33.107:9000/api/measures/component?metricKeys=vulnerabilities&component=comrades.bmi%3ABMI'
+sh 'curl -X GET -u admin:admin http://3.16.33.107:9000/api/measures/component?metricKeys=vulnerabilities&component=comrades.bmi%3ABMI '
 }
