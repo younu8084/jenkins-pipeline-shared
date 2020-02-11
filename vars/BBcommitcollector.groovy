e29639923eb8a2dc9fa78276503ea412a4b8d502
REVATHISARAISWATHI <36625520+REVATHISARAISWATHI@users.noreply.github.com>
2020-02-11 19:19:24 +0530

    Update BBcommitcollector.groovy

--- vars/BBcommitcollector.groovy
+++ vars/BBcommitcollector.groovy
@@ -1,4 +1,12 @@
-def call()
-{
-sh 'curl -v -G --user rig:rigaDapt@devOps http://18.224.68.30:7990/rest/api/1.0/projects/edn/repos/rig/commits'
+def call(jsondata){
+def jsonString = jsondata
+def jsonObj = readJSON text: jsonString
+
+String b=jsonObj.scm.projects.project.project_key 
+String Key=b.replaceAll("\\[", "").replaceAll("\\]","");
+println(Key)
+ withCredentials([usernamePassword(credentialsId: 'bitbucket_cred', passwordVariable: 'pass', usernameVariable: 'userId')]) {
+
+sh "curl -X GET  -H -d  -u  $userId:$pass http://18.224.68.30:7990/rest/api/1.0/projects/'${Key}'/repos/rig/commits" 
+ }
 }
