9ae1dec2203581adcef57a4a5bc7a73b61c2a397
REVATHISARAISWATHI <36625520+REVATHISARAISWATHI@users.noreply.github.com>
2020-02-11 20:09:57 +0530

    Update Asonar.groovy

--- vars/Asonar.groovy
+++ vars/Asonar.groovy
@@ -1,4 +1,5 @@
-def call(){
-def process=sh 'curl --location --request GET http://3.16.33.107:9000/api/measures/component?metricKeys=vulnerabilities&component=comrades.bmi%3ABMI \
---header 'Authorization: Basic YWRtaW46YWRtaW4= -o ouput.json''
+def call()
+{
+def json= sh 'curl -v -G --user admin:admin http://3.16.33.107:9000/api/measures/component?metricKeys=vulnerabilities&component=comrades.bmi%3ABMI -o ouput.json'
+ 
 }
