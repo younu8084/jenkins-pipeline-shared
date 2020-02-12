fa28ffad9478489735c69fd4ea26e886f99d8e20
REVATHISARAISWATHI <36625520+REVATHISARAISWATHI@users.noreply.github.com>
2020-02-12 12:16:55 +0530

    Create CCinflux.groovy

---
+++ vars/CCinflux.groovy
@@ -0,0 +1,16 @@
+import groovy.json.*
+
+@NonCPS
+create(){
+def jsonSlurper = new JsonSlurper()
+def reader = new BufferedReader(new InputStreamReader(new FileInputStream("/var/lib/jenkins/workspace/${JOB_NAME}/ouput.json"),"UTF-8"))
+def resultJson = jsonSlurper.parse(reader)
+def value = resultJson.component.measures[0].value
+ 
+sh """curl -i -XPOST 'http://ec2-13-58-47-71.us-east-2.compute.amazonaws.com:8086/write?db=Collector' --data-binary 'sonar coverage=${value}' 
+"""
+}
+def call()
+{
+ create()
+}
