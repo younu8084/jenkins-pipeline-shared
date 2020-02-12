e8cff2cb2e480a71c8c0e7ff62b504d456612b9f
REVATHISARAISWATHI <36625520+REVATHISARAISWATHI@users.noreply.github.com>
2020-02-12 12:36:40 +0530

    Update BBusers.groovy

--- vars/BBusers.groovy
+++ vars/BBusers.groovy
@@ -6,7 +6,7 @@ String b=jsonObj.scm.projects.project.project_key
 String Key=b.replaceAll("\\[", "").replaceAll("\\]","");
 println(Key)
  withCredentials([usernamePassword(credentialsId: 'bitbucket_cred', passwordVariable: 'pass', usernameVariable: 'userId')]) {
- def json= sh "curl -s  GET  --user  $userId:$pass http://18.224.68.30:7990/rest/api/1.0/projects/${Key}/permissions/users -o output.json"
+ def json= sh "curl -s  GET   http://18.224.68.30:7990/rest/api/1.0/projects/'${Key}'/permissions/users --user  $userId:$pass -o output.json"
  println(json)
  }
 }
