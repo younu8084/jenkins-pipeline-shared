bb8a28a4e3dc20ae389d63c88666989e53ac2863
REVATHISARAISWATHI <36625520+REVATHISARAISWATHI@users.noreply.github.com>
2020-02-12 14:21:42 +0530

    Update BBpull.groovy

--- vars/BBpull.groovy
+++ vars/BBpull.groovy
@@ -1,1 +1,14 @@
+def call(jsondata){
+def jsonString = jsondata
+def jsonObj = readJSON text: jsonString
 
+String b=jsonObj.scm.projects.project.project_key 
+String Key=b.replaceAll("\\[", "").replaceAll("\\]","");
+println(Key)
+ withCredentials([usernamePassword(credentialsId: 'bitbucket_cred', passwordVariable: 'pass', usernameVariable: 'userId')]) {
+  sh "curl -X GET  -H -d  -u  $userId:$pass http://18.224.68.30:7990/rest/api/1.0/projects/EDN/repos/rig/pull-requests -o output.json"
+
+
+ 
+ }
+}
