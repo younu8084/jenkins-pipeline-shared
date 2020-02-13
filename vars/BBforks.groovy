93aac710ac839cef46362139619b64cef4328b41
REVATHISARAISWATHI <36625520+REVATHISARAISWATHI@users.noreply.github.com>
2020-02-13 16:32:57 +0530

    Update BBforks.groovy

--- vars/BBforks.groovy
+++ vars/BBforks.groovy
@@ -7,9 +7,11 @@ String b=jsonObj.scm.projects.project.project_key
 String Key=b.replaceAll("\\[", "").replaceAll("\\]","");
 println(Key)
  withCredentials([usernamePassword(credentialsId: 'bitbucket_cred', passwordVariable: 'pass', usernameVariable: 'userId')]) {
-def response =  sh "curl -X GET  -H -d  -u  $userId:$pass http://18.224.68.30:7990/rest/api/1.0/projects/${Key}/repos/${repoName}/forks -o output.json"
+def response =  sh "curl -w '%{http_code}' -s -i -X GET  -H -d  -u  $userId:$pass http://18.224.68.30:7990/rest/api/1.0/projects/${Key}/repos/${repoName}/forks -o test.txt"
  }
-println($response)
+def response =new File('/var/lib/jenkins/workspace/sonarnew/test.txt').text
+ 
+  echo " ============ $response"
  if(response == "204" || response == "200")
 {
  echo " Forks are listed successfully "
