920f97a361634631abb8c196326976d047125722
REVATHISARAISWATHI <36625520+REVATHISARAISWATHI@users.noreply.github.com>
2020-02-13 16:26:55 +0530

    Update Ainflux1.groovy

--- vars/Ainflux1.groovy
+++ vars/Ainflux1.groovy
@@ -1,12 +1,10 @@
 import groovy.json.*
 
 @NonCPS
-create(){
 def jsonSlurper = new JsonSlurper()
-def reader = new BufferedReader(new InputStreamReader(new FileInputStream("/var/lib/jenkins/workspace/${JOB_NAME}/ouput.json"),"UTF-8"))
-def resultJson = jsonSlurper.parse(reader)
-def value = resultJson.component.measures[0].value
- 
+
+def pushToInflux(totalIssues)
+{
  sh """curl -i -w '%{http_code}' -XPOST 'http://3.16.33.107:9000/api/measures/component?metricKeys=vulnerabilities&component=comrades.bmi%3ABMI'  --header 'Authorization: Basic YWRtaW46YWRtaW4= ' >test.txt"""
 
  def response =new File('/var/lib/jenkins/workspace/sonarnew/test.txt').text
@@ -22,8 +20,4 @@ else
 {
  error("Error while pushing")
 }
-}
-def call()
-{
- create()
 }
