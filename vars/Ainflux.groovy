8084bc88f863f3a9b88d987ef0a96d04f7fbdaa5
REVATHISARAISWATHI <36625520+REVATHISARAISWATHI@users.noreply.github.com>
2020-02-13 12:48:22 +0530

    Update Ainflux.groovy

--- vars/Ainflux.groovy
+++ vars/Ainflux.groovy
@@ -8,9 +8,9 @@ def reader = new BufferedReader(new InputStreamReader(new FileInputStream("/var/
 def resultJson = jsonSlurper.parse(reader)
 def value = resultJson.component.measures[0].value
  
-sh """curl -i -XPOST 'http://ec2-13-58-47-71.us-east-2.compute.amazonaws.com:8086/write?db=Collector' --data-binary 'sonar vulnerabilities=${value}' >output.txt"""
+sh """curl -i -XPOST 'http://ec2-13-58-47-71.us-east-2.compute.amazonaws.com:8086/write?db=Collector' --data-binary 'sonar vulnerabilities=${value}' >test.txt"""
 
- def response =new File('/var/lib/jenkins/workspace/sonarnew/output.txt').text
+ def response =new File('/var/lib/jenkins/workspace/sonarnew/test.txt').text
 echo "========================= $response"
 
 if(response == "204" || response == "200")
