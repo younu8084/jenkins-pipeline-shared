f5f6e53df93c213aeeda3347460a8cc9e91d0987
REVATHISARAISWATHI <36625520+REVATHISARAISWATHI@users.noreply.github.com>
2020-02-13 12:45:38 +0530

    Update Ainflux.groovy

--- vars/Ainflux.groovy
+++ vars/Ainflux.groovy
@@ -8,10 +8,19 @@ def reader = new BufferedReader(new InputStreamReader(new FileInputStream("/var/
 def resultJson = jsonSlurper.parse(reader)
 def value = resultJson.component.measures[0].value
  
-sh """curl -i -XPOST 'http://ec2-13-58-47-71.us-east-2.compute.amazonaws.com:8086/write?db=Collector' --data-binary 'sonar vulnerabilities=${value}' 
-"""
-}
-def call()
+sh """curl -i -XPOST 'http://ec2-13-58-47-71.us-east-2.compute.amazonaws.com:8086/write?db=Collector' --data-binary 'sonar vulnerabilities=${value}' >output.txt"""
+
+ def response =new File('/var/lib/jenkins/workspace/sonarnew/output.txt').text
+echo "========================= $response"
+
+if(response == "204" || response == "200")
 {
- create()
+ echo " Data pushed into influxDB "
 }
+else
+{
+ error("Error while pushing")
+}
+}
+
+
