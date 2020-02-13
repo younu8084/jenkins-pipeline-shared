e57a0123f9411161374770a7248c129260e5a583
REVATHISARAISWATHI <36625520+REVATHISARAISWATHI@users.noreply.github.com>
2020-02-13 16:35:15 +0530

    Update Ainflux1.groovy

--- vars/Ainflux1.groovy
+++ vars/Ainflux1.groovy
@@ -1,8 +1,5 @@
 def call ()
 {
- def jsonSlurper = new JsonSlurper()
-def reader = new BufferedReader(new InputStreamReader(new FileInputStream("/var/lib/jenkins/workspace/${JOB_NAME}/ouput.json"),"UTF-8"))
-def resultJson = jsonSlurper.parse(reader)
  sh """curl -i -w '%{http_code}' -XPOST 'http://3.16.33.107:9000/api/measures/component?metricKeys=vulnerabilities&component=comrades.bmi%3ABMI'  --header 'Authorization: Basic YWRtaW46YWRtaW4= ' >test.txt"""
 
  def response =new File('/var/lib/jenkins/workspace/sonarnew/test.txt').text
@@ -12,7 +9,7 @@ def resultJson = jsonSlurper.parse(reader)
 
 if(response == "200")
 {
- echo "Data pushed into influxDB"
+ echo ("Data pushed into influxDB")
 }
 else
 {
