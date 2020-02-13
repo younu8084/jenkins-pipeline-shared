cb9067ae2d52bd22c1a2ce8eed1b2dd7f687ff61
REVATHISARAISWATHI <36625520+REVATHISARAISWATHI@users.noreply.github.com>
2020-02-13 17:11:57 +0530

    Update Ainflux.groovy

--- vars/Ainflux.groovy
+++ vars/Ainflux.groovy
@@ -4,10 +4,15 @@ import groovy.json.*
 @NonCPS
 create(){
 def jsonSlurper = new JsonSlurper()
+ def value = resultJson.component.measures[0].value
+ def value = resultJson.value
+ pushToInflux(value);
+}
+def pushToInflux(value)
  
  sh """curl -i -w '%{http_code}' -XPOST 'http://ec2-13-58-47-71.us-east-2.compute.amazonaws.com:8086/write?db=Collector' --data-binary 'sonar vulnerabilities=${value}' >test.txt"""
 
- def response =new File('/var/lib/jenkins/workspace/sonarnew/test.txt').text
+ def response =new File('/var/lib/jenkins/workspace/' + JOB_NAME+ '/test.txt').text
 def value = resultJson.component.measures[0].value
  echo " ============ $response"
 
