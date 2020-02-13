44e0e0580f9a3b737dff9b01ac87242ea7801644
REVATHISARAISWATHI <36625520+REVATHISARAISWATHI@users.noreply.github.com>
2020-02-13 19:42:40 +0530

    Update Ainflux.groovy

--- vars/Ainflux.groovy
+++ vars/Ainflux.groovy
@@ -6,12 +6,15 @@ create(){
 def jsonSlurper = new JsonSlurper()
  def resultJson = jsonSlurper.parse(new File("/var/lib/jenkins/workspace/${JOB_NAME}/ouput.json"))
  def value = resultJson.component.measures[0].value
- 
 pushToInflux(value);
 }
 def pushToInflux(value)
 {
- sh """curl -w '%{http_code}' -s -i -X POST 'http://ec2-13-58-47-71.us-east-2.compute.amazonaws.com:8086/write?db=Collector' --data 'sonar vulnerabilities=${value}' > test.txt """
+ sh """
+    curl -w '%{http_code}' -s -i  -X POST \
+      'http://ec2-13-58-47-71.us-east-2.compute.amazonaws.com:8086/write?db=Collector' \
+      --data 'sonar vulnerabilities=${value}' > test.txt
+  """
 
 def response =new File('/var/lib/jenkins/workspace/' + JOB_NAME+ '/test.txt')
 
