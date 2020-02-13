e07eab44868a209e1282a5c1647bfe219ea7b455
REVATHISARAISWATHI <36625520+REVATHISARAISWATHI@users.noreply.github.com>
2020-02-13 13:00:09 +0530

    Update Ainflux.groovy

--- vars/Ainflux.groovy
+++ vars/Ainflux.groovy
@@ -9,7 +9,7 @@ def resultJson = jsonSlurper.parse(reader)
 def value = resultJson.component.measures[0].value
  
 sh """curl -i -XPOST 'http://ec2-13-58-47-71.us-east-2.compute.amazonaws.com:8086/write?db=Collector' --data-binary 'sonar vulnerabilities=${value}' >test.txt"""
-}
+
  def response =new File('/var/lib/jenkins/workspace/sonarnew/test.txt').text
 echo "========================= $response"
 
@@ -21,7 +21,7 @@ else
 {
  error("Error while pushing")
 }
-
+}
 def call()
 {
  create()
