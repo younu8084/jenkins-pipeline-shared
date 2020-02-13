5be38c1ebe672931401816fa81ab816f03016463
REVATHISARAISWATHI <36625520+REVATHISARAISWATHI@users.noreply.github.com>
2020-02-13 19:34:41 +0530

    Update Ainflux.groovy

--- vars/Ainflux.groovy
+++ vars/Ainflux.groovy
@@ -12,7 +12,7 @@ def pushToInflux(value)
 {
  sh """curl -w '%{http_code}' -s -i -X POST 'http://ec2-13-58-47-71.us-east-2.compute.amazonaws.com:8086/write?db=Collector' --data 'sonar vulnerabilities=${value}' > test.txt """
 
-def response =new File('/var/lib/jenkins/workspace/' + JOB_NAME+ '/test.txt').text
+def response =new File('/var/lib/jenkins/workspace/' + JOB_NAME+ '/test.txt')
 
  echo " ============ $response"
 
