2bca5c8eb876fdc6fb3500ef5fd526a6c327f912
REVATHISARAISWATHI <36625520+REVATHISARAISWATHI@users.noreply.github.com>
2020-02-14 15:43:27 +0530

    Update Influxdb.groovy

--- vars/Influxdb.groovy
+++ vars/Influxdb.groovy
@@ -3,13 +3,13 @@ import groovy.json.*
 @NonCPS
 create(){
 def jsonSlurper = new JsonSlurper()
-def reader = new BufferedReader(new InputStreamReader(new FileInputStream("/var/lib/jenkins/workspace/${JOB_NAME}/output.json"),"UTF-8"))
+def reader = new BufferedReader(new InputStreamReader(new FileInputStream("/var/lib/jenkins/workspace/${JOB_NAME}/test.txt"),"UTF-8"))
 def resultJson = jsonSlurper.parse(reader)
 def total = resultJson.size
   echo "$total"
   
-sh """curl -i -XPOST 'http://ec2-13-58-47-71.us-east-2.compute.amazonaws.com:8086/write?db=Collector' --data-binary 'bitbucket pullrequest=${total}' 
-"""
+//sh """curl -i -XPOST 'http://ec2-13-58-47-71.us-east-2.compute.amazonaws.com:8086/write?db=Collector' --data-binary 'bitbucket pullrequest=${total}' 
+//"""
 }
 
 
