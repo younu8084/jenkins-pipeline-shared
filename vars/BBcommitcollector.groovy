def8b5678d0c0831235ec3539c9fe3d0dce3f8d2
REVATHISARAISWATHI <36625520+REVATHISARAISWATHI@users.noreply.github.com>
2020-02-24 15:18:37 +0530

    Update BBcommitcollector.groovy

--- vars/BBcommitcollector.groovy
+++ vars/BBcommitcollector.groovy
@@ -11,8 +11,8 @@ println(Key)
 println(repoName)
  Date date = new Date() 
  withCredentials([usernamePassword(credentialsId: 'bitbucket_cred', passwordVariable: 'pass', usernameVariable: 'userId')]) {
-  sh "curl -X GET  -H -d  -u  rig:rigaDapt@devOps  http://18.224.68.30:7990/rest/api/1.0/projects/EDN/repos/demo12/commits  -o output.json"
-  
+  sh "curl -X GET  -H -d  -u $userId:$pass http://18.224.68.30:7990/rest/api/1.0/projects/EDN/repos/demo12/commits  -o output.json"
+ } 
 
  
  }
