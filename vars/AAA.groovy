171ea1c12e243cde5c157847f343dd04963038c7
REVATHISARAISWATHI <36625520+REVATHISARAISWATHI@users.noreply.github.com>
2020-02-24 13:07:07 +0530

    Create AAA.groovy

---
+++ vars/AAA.groovy
@@ -0,0 +1,23 @@
+import groovy.json.*
+
+@NonCPS
+create(){
+  def jsonSlurper = new JsonSlurper()
+  def resultJson = jsonSlurper.parse(new File("/var/lib/jenkins/workspace/${JOB_NAME}/output.json"))
+ def value = resultJson.values
+ println(value)
+ for(i=0;i<=value;i++)
+ {
+  if(resultJson.author.name=="rig")
+  {
+   def count=0
+   count ++
+   }
+ }
+  
+ 
+}
+def call()
+{
+create()
+}
