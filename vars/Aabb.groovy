a29d39e625896f5676a35f71225b10edbd4ebeaa
REVATHISARAISWATHI <36625520+REVATHISARAISWATHI@users.noreply.github.com>
2020-02-24 14:39:13 +0530

    Update Aabb.groovy

--- vars/Aabb.groovy
+++ vars/Aabb.groovy
@@ -35,7 +35,7 @@ println(nobuild)
 
 def success(totalbuilds)
 {
-   sh sh 'curl -X GET  -H -d  -u rig:rigaDapt@devOps "http://18.224.68.30:7990/rest/api/1.0/projects/EDN/repos/rig/commits" -o ouput.json' 
+   sh sh 'curl -X GET  -H -d  -u rig:rigaDapt@devOps "http://18.224.68.30:7990/rest/api/1.0/projects/EDN/repos/demo12/commits" -o ouput.json' 
     def jsonSlurper = new JsonSlurper()
 def reader = new BufferedReader(new InputStreamReader(new FileInputStream("/var/lib/jenkins/workspace/${JOB_NAME}/ouput.json"),"UTF-8"))
 def resultJson = jsonSlurper.parse(reader)
