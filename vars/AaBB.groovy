3d993c868ce6efdbc325de88ccdd85f0107c27fb
REVATHISARAISWATHI <36625520+REVATHISARAISWATHI@users.noreply.github.com>
2020-02-24 15:50:53 +0530

    Update AaBB.groovy

--- vars/AaBB.groovy
+++ vars/AaBB.groovy
@@ -1,4 +1,4 @@
 def call()
 {
-  sh "curl -X GET -s -u rig:rigaDapt@devOps http://18.224.68.30:7990/rest/api/1.0/projects/EDN/repos/demo12/commits?merges=exclude&since=f01525bb2d3&until=f01525bb2d3 -o ouput.json"
+  sh "curl -X GET -s -u rig:rigaDapt@devOps http://18.224.68.30:7990/rest/api/1.0/projects/EDN/repos/demo12/commits?merges=exclude&since=f01525bb2d3&until=f01525bb2d3 "
 }
