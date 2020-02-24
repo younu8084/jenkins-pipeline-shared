d4d96dbc4935c81740245885a952fe5def9e8850
REVATHISARAISWATHI <36625520+REVATHISARAISWATHI@users.noreply.github.com>
2020-02-24 15:11:37 +0530

    Update AAA.groovy

--- vars/AAA.groovy
+++ vars/AAA.groovy
@@ -3,6 +3,9 @@ import groovy.json.*
 @NonCPS
 create(){
   def jsonSlurper = new JsonSlurper()
+  
+  def json = JSON.stringify(date);
+  
   def resultJson = jsonSlurper.parse(new File("/var/lib/jenkins/workspace/${JOB_NAME}/output.json"))
 def total = resultJson.size
   echo "$total"
