c8fdeae46fa8dff51bf0e460db87f13c8197835e
REVATHISARAISWATHI <36625520+REVATHISARAISWATHI@users.noreply.github.com>
2020-02-24 17:11:41 +0530

    Update AaBB.groovy

--- vars/AaBB.groovy
+++ vars/AaBB.groovy
@@ -5,7 +5,7 @@ import java.util.Date.*
 @NonCPS
 create(){
   def jsonSlurper = new JsonSlurper()
-  def resultJson = jsonSlurper.parse(new File("/var/lib/jenkins/workspace/${JOB_NAME}/output.json"))
+  def resultJson = jsonSlurper.parse(new File("/var/lib/jenkins/workspace/${JOB_NAME}/ouput.json"))
  // def String="timestamp"
 def total = resultJson.size
   echo "$total"
