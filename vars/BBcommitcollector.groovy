0d266caeb51b7c1356fdb100773fcd2a684a9f98
REVATHISARAISWATHI <36625520+REVATHISARAISWATHI@users.noreply.github.com>
2020-02-24 10:38:47 +0530

    Update BBcommitcollector.groovy

--- vars/BBcommitcollector.groovy
+++ vars/BBcommitcollector.groovy
@@ -7,10 +7,12 @@ String b=jsonObj.scm.projects.project.project_key
 String Key=b.replaceAll("\\[", "").replaceAll("\\]","");
 println(Key)
 println(repoName)
+ Date date = new Date() 
  withCredentials([usernamePassword(credentialsId: 'bitbucket_cred', passwordVariable: 'pass', usernameVariable: 'userId')]) {
   sh "curl -X GET  -H -d  -u  $userId:$pass http://18.224.68.30:7990/rest/api/1.0/projects/'${Key}'/repos/'${repoName}'/commits -o output.json"
-
+  echo "git commitsAtDate '${date}'"
 
  
  }
+ 
 }
