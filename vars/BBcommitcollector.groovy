ef7607e29e1a4e8da9f2bf7aa13bf040e851f4b6
REVATHISARAISWATHI <36625520+REVATHISARAISWATHI@users.noreply.github.com>
2020-02-24 12:27:52 +0530

    Update BBcommitcollector.groovy

--- vars/BBcommitcollector.groovy
+++ vars/BBcommitcollector.groovy
@@ -1,14 +1,4 @@
-def call(jsondata){
-def jsonString = jsondata
-def jsonObj = readJSON text: jsonString
-String a=jsonObj.scm.projects.project.repositories.repository.repo_name
-String repoName=a.replaceAll("\\[", "").replaceAll("\\]","");
-String b=jsonObj.scm.projects.project.project_key 
-String Key=b.replaceAll("\\[", "").replaceAll("\\]","");
-println(Key)
-println(repoName)
+def call(){
  Date date = new Date() 
-
-  sh "curl -X GET  -H -d  -u  rig:rigaDapt@devOps http://18.224.68.30:7990/rest/api/1.0/projects/EDN/repos/demo12/users/rig/commits?since='${date}'&until='${date}' -o output.json"
- 
-}
+sh "curl -X GET  -H -d  -u  rig:rigaDapt@devOps http://18.224.68.30:7990/rest/api/1.0/projects/EDN/repos/demo12/users/rig/commits?since='${date}'&until='${date}' "
+ }
