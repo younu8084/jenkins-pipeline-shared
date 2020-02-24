8474ea83fa006c6d4392d7994c1a30f703c4ce40
REVATHISARAISWATHI <36625520+REVATHISARAISWATHI@users.noreply.github.com>
2020-02-24 16:22:33 +0530

    Update AaBB.groovy

--- vars/AaBB.groovy
+++ vars/AaBB.groovy
@@ -1,19 +1,20 @@
 
 import groovy.json.*
 import java.util.*
-import java.sql.timestamp.*   
+import java.sql.Timestamp.*   
 import java.util.Date.*
 @NonCPS
 create(){
   def jsonSlurper = new JsonSlurper()
   def resultJson = jsonSlurper.parse(new File("/var/lib/jenkins/workspace/${JOB_NAME}/output.json"))
+ // def String="timestamp"
 def total = resultJson.size
   echo "$total"
 def value=resultJson.values.author[0].name
   echo "$value"
  // Date date = new Date() 
  // echo "$date"
- def timestamp ts=new timestamp(System.currentTimeMillis());  
+ def Timestamp ts=new Timestamp(System.currentTimeMillis());  
   Date date=new Date(ts.getTime());  
   echo "$date"
   def count=0
