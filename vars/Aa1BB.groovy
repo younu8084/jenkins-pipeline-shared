3ca749bdb1418021051088daec000034713cda88
REVATHISARAISWATHI <36625520+REVATHISARAISWATHI@users.noreply.github.com>
2020-02-24 17:33:57 +0530

    Update Aa1BB.groovy

--- vars/Aa1BB.groovy
+++ vars/Aa1BB.groovy
@@ -3,16 +3,14 @@ import groovy.json.*
 def call(jsondata){
 def jsonString = jsondata
 def jsonObj = readJSON text: jsonString
-String a=jsonObj.scm.projects.project.repositories.repository.repo_name
-String repoName=a.replaceAll("\\[", "").replaceAll("\\]","");
-String b=jsonObj.scm.projects.project.project_key 
-String Key=b.replaceAll("\\[", "").replaceAll("\\]","");
-println(Key)
-println(repoName)
+//String a=jsonObj.scm.projects.project.repositories.repository.repo_name
+//String repoName=a.replaceAll("\\[", "").replaceAll("\\]","");
+//String b=jsonObj.scm.projects.project.project_key 
+//String Key=b.replaceAll("\\[", "").replaceAll("\\]","");
+//println(Key)
+//println(repoName)
  Date date = new Date() 
  withCredentials([usernamePassword(credentialsId: 'bitbucket_cred', passwordVariable: 'pass', usernameVariable: 'userId')]) {
-  sh "curl -X GET  -H -d  -u $userId:$pass http://18.224.68.30:7990/rest/api/1.0/projects/'${Key}'/repos/'${repoName}'/commits -o ouput.json"
+  sh "curl -X GET  -H -d  -u $userId:$pass http://18.224.68.30:7990/rest/api/1.0/projects/EDN/repos/demo12/commits -o ouput.json"
  } 
-
- 
- }
+}
