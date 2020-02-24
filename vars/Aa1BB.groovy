fbea1c10038f4563b51c26588ba36edca3f92b63
REVATHISARAISWATHI <36625520+REVATHISARAISWATHI@users.noreply.github.com>
2020-02-24 17:13:02 +0530

    Create Aa1BB.groovy

---
+++ vars/Aa1BB.groovy
@@ -0,0 +1,18 @@
+import groovy.json.*
+
+def call(jsondata){
+def jsonString = jsondata
+def jsonObj = readJSON text: jsonString
+String a=jsonObj.scm.projects.project.repositories.repository.repo_name
+String repoName=a.replaceAll("\\[", "").replaceAll("\\]","");
+String b=jsonObj.scm.projects.project.project_key 
+String Key=b.replaceAll("\\[", "").replaceAll("\\]","");
+println(Key)
+println(repoName)
+ Date date = new Date() 
+ withCredentials([usernamePassword(credentialsId: 'bitbucket_cred', passwordVariable: 'pass', usernameVariable: 'userId')]) {
+  sh "curl -X GET  -H -d  -u $userId:$pass http://18.224.68.30:7990/rest/api/1.0/projects/'${Key}'/repos/'${repoName}'/commits -o ouput.json"
+ } 
+
+ 
+ }
