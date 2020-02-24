ec079a5b13804e3d3b480eff774fbe0551da5b8d
REVATHISARAISWATHI <36625520+REVATHISARAISWATHI@users.noreply.github.com>
2020-02-24 13:04:09 +0530

    Update BBcommitcollector.groovy

--- vars/BBcommitcollector.groovy
+++ vars/BBcommitcollector.groovy
@@ -16,23 +16,6 @@ println(repoName)
 
  
  }
- create()
+
 }
-@NonCPS
-def create()
-{
-  def jsonSlurper = new JsonSlurper()
-  def resultJson = jsonSlurper.parse(new File("/var/lib/jenkins/workspace/${JOB_NAME}/output.json"))
- def value = resultJson.values
- println(value)
- for(i=0;i<=value;i++)
- {
-  if(resultJson.author.name=="rig")
-  {
-   def count=0
-   count ++
-   }
- }
-  
- 
-}
+
