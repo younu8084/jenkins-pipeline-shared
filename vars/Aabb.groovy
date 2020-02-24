0d36ac884367574588a9216146087ae50d9f8235
REVATHISARAISWATHI <36625520+REVATHISARAISWATHI@users.noreply.github.com>
2020-02-24 14:46:43 +0530

    Update Aabb.groovy

--- vars/Aabb.groovy
+++ vars/Aabb.groovy
@@ -1,51 +1,17 @@
 import groovy.json.*
-
-int totalbuilds;
-
 def call()
 {
-sh 'curl -X GET  -H -d  -u rig:rigaDapt@devOps "http://18.224.68.30:7990/rest/api/1.0/projects/EDN/repos/demo12/commits" -o ouput.json' 
- def jsonSlurper = new JsonSlurper()
+  sh "curl -X GET -s -u rig:rigaDapt@devOps http://18.224.68.30:7990/rest/api/1.0/projects/EDN/repos/rig/commits -o ouput.json"
+  def jsonSlurper = new JsonSlurper()
 def reader = new BufferedReader(new InputStreamReader(new FileInputStream("/var/lib/jenkins/workspace/${JOB_NAME}/ouput.json"),"UTF-8"))
 def resultJson = jsonSlurper.parse(reader)
-def nobuild=resultJson.imageMap
- // env.name = nobuild
-
-println(nobuild)
-  if(nobuild!=null)
-  {
-    def val = nobuild.split('title=\"')
-   // println(val[1])
-   //def val1=val[1]
-     //println(val1)
-     def vals = val[1].split(' ')
-    def totalbuild=vals[3]
-    println(totalbuild)
-  // success(totalbuild);
-    return totalbuild
+  def size = resultJson.size()
+  for(i=0;i<size;i++){
+def uname=resultJson[i].name
+  println(uname)
+def commit=resultJson[i].commits
+println(commit)
   }
-  else
-  {
-    
-    echo "the no of build is 0" 
-    return 0
-  }
- 
-}
-
-def success(totalbuilds)
-{
-   sh sh 'curl -X GET  -H -d  -u rig:rigaDapt@devOps "http://18.224.68.30:7990/rest/api/1.0/projects/EDN/repos/demo12/commits" -o ouput.json' 
-    def jsonSlurper = new JsonSlurper()
-def reader = new BufferedReader(new InputStreamReader(new FileInputStream("/var/lib/jenkins/workspace/${JOB_NAME}/ouput.json"),"UTF-8"))
-def resultJson = jsonSlurper.parse(reader)
-    def nbuild=resultJson.imageMap
-      def val2 = nbuild.split('title=\"')
-     def builds = val2[1].split(' ')
-    def Failbuild=builds[2]
-    println(Failbuild)
-  println(totalbuilds)
-  //def var=totalbuilds
-  def successbuild=totalbuilds.toInteger()-Failbuild.toInteger()
-     println(successbuild)
+  //ef val=arr  { reg
+  //println(val)
 }
