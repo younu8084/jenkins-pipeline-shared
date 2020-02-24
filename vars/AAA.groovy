d2c6adece407ab0f278e0f394f66b1cf034926d4
REVATHISARAISWATHI <36625520+REVATHISARAISWATHI@users.noreply.github.com>
2020-02-24 17:12:29 +0530

    Update AAA.groovy

--- vars/AAA.groovy
+++ vars/AAA.groovy
@@ -10,20 +10,24 @@ def total = resultJson.size
   echo "$total"
 def value=resultJson.values.author[0].name
   echo "$value"
- Long timer=resultJson.values.committerTimestamp[0]
-  echo "$timer"
+
+  
   
  Long date= System.currentTimeMillis() 
   echo "$date"
   
-  Long sub=Math.subtractExact(date,timer)
-  echo "$sub"
+  
   def count=0
   //
+ 
  for(i=0;i<total;i++)
  {
+    Long timer=resultJson.values.committerTimestamp[i]
+   echo "$timer"
+   Long sub=Math.subtractExact(date,timer)
+  echo "$sub"
    //if(resultJson.values.committerTimestamp[i]==1582522990000)
-   if (resultJson.values.committerTimestamp[i]==date)
+   if (sub<86400000)
    {
     count ++
    echo "$count"
