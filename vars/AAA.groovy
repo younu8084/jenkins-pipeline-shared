1737455ded2b0de9cb866bf5cb4cc5b2fe9c9dce
REVATHISARAISWATHI <36625520+REVATHISARAISWATHI@users.noreply.github.com>
2020-02-24 14:55:09 +0530

    Update AAA.groovy

--- vars/AAA.groovy
+++ vars/AAA.groovy
@@ -14,7 +14,7 @@ def timer=resultJson.values.committerTimestamp[0]
   Date date = new Date() 
  for(i=0;i<total;i++)
  {
-   if( resultJson.values.committerTimestamp[i]=="${date}")
+   if( resultJson.values.committerTimestamp[i]=="1582522990000")
   {
     count ++
    echo "$count"
