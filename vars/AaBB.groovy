c8b279e45d865bcfcf6625343ed01974760a92f4
REVATHISARAISWATHI <36625520+REVATHISARAISWATHI@users.noreply.github.com>
2020-02-24 17:58:44 +0530

    Update AaBB.groovy

--- vars/AaBB.groovy
+++ vars/AaBB.groovy
@@ -14,10 +14,46 @@ def value=resultJson.values.author[0].name
   echo "$value"
  // Date date = new Date() 
  // echo "$date"
-  Timestamp ts=new Timestamp(System.currentTimeMillis());  
-  Date date=ts;  
+ // Timestamp ts=new Timestamp(System.currentTimeMillis());  
+  //Date date=ts;  
 //  def Timestamp = resultJson.values.committerTimestamp[0]
  // Date date= new Date(Timestamp);  
+  function convert(){
+
+ // Unixtimestamp
+ var unixtimestamp = document.getElementById('committerTimestamp').value;
+
+ // Months array
+ var months_arr = ['Jan','Feb','Mar','Apr','May','Jun','Jul','Aug','Sep','Oct','Nov','Dec'];
+
+ // Convert timestamp to milliseconds
+ var date = new Date(unixtimestamp*1000);
+
+ // Year
+ var year = date.getFullYear();
+
+ // Month
+ var month = months_arr[date.getMonth()];
+
+ // Day
+ var day = date.getDate();
+
+ // Hours
+ var hours = date.getHours();
+
+ // Minutes
+ var minutes = "0" + date.getMinutes();
+
+ // Seconds
+ var seconds = "0" + date.getSeconds();
+
+ // Display date time in MM-dd-yyyy h:m:s format
+ var convdataTime = month+'-'+day+'-'+year+' '+hours + ':' + minutes.substr(-2) + ':' + seconds.substr(-2);
+ 
+ document.getElementById('datetime').innerHTML = convdataTime;
+ 
+}
+  
   echo "$date"
   def count=0
   //
@@ -34,5 +70,6 @@ def value=resultJson.values.author[0].name
 def call()
 {
 create()
+  function convert()
 }
 
