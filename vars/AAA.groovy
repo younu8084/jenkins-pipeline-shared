8331dc3c97e80812ce009aa4a4f7cf9e43eb8e05
REVATHISARAISWATHI <36625520+REVATHISARAISWATHI@users.noreply.github.com>
2020-02-24 13:16:19 +0530

    Update AAA.groovy

--- vars/AAA.groovy
+++ vars/AAA.groovy
@@ -6,8 +6,8 @@ create(){
   def resultJson = jsonSlurper.parse(new File("/var/lib/jenkins/workspace/${JOB_NAME}/output.json"))
 def total = resultJson.size
   echo "$total"
-  def new=resultJson.author[0].name[0]
-  echo "$new"
+  def totall=resultJson.author[0].name[0]
+  echo "$totall"
  for(i=0;i<=total;i++)
  {
   if(resultJson.author[i].name[i]=="rig")
