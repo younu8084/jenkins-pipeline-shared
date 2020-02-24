5c82d61343b8b869095baf697e686c876e60f58a
REVATHISARAISWATHI <36625520+REVATHISARAISWATHI@users.noreply.github.com>
2020-02-24 16:44:09 +0530

    Update AaBB.groovy

--- vars/AaBB.groovy
+++ vars/AaBB.groovy
@@ -13,7 +13,7 @@ def value=resultJson.values.author[0].name
   echo "$value"
  // Date date = new Date() 
  // echo "$date"
- def Timestamp = resultJson.authorTimestamp
+  def Timestamp = resultJson.authorTimestamp[0]
   Date date= new Date(Timestamp);  
   echo "$date"
   def count=0
