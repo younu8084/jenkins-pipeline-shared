69310aea2e7eef12df578349aa04a2d8f1319b03
REVATHISARAISWATHI <36625520+REVATHISARAISWATHI@users.noreply.github.com>
2020-02-25 18:34:36 +0530

    Update BBcommitcollector.groovy

--- vars/BBcommitcollector.groovy
+++ vars/BBcommitcollector.groovy
@@ -28,7 +28,7 @@ for(i=0;i<ecount;i++)
   {
    if(jsonObj.config.emails.email[i]==resultJson.values.author[j].emailAddress)
    {
-	JSON(JsonOutput.toJson(resultJson.values[j])) 
+	JSON.add(JsonOutput.toJson(resultJson.values[j])) 
     
     }
 	  
