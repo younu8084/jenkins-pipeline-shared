f06730bb4aeff716bf334b0db56109c27ede748a
REVATHISARAISWATHI <36625520+REVATHISARAISWATHI@users.noreply.github.com>
2020-02-25 13:08:01 +0530

    Update BBcommitcollector.groovy

--- vars/BBcommitcollector.groovy
+++ vars/BBcommitcollector.groovy
@@ -26,9 +26,7 @@ for(i=0;i<ecount;i++)
   {
    if(jsonObj.config.emails.email[i]==resultJson.values.author[j].emailAddress)
    {
-    y = resultJson.values[j];
-    println(y)
-    sh "echo '${y}' >>commiter'${commiter}'.txt "
+     sh "echo resultJson.values[j] >>commiter'${commiter}'.json "
     }
 	  //Long commitdate=resultJson.values.committerTimestamp[j]
 	  //def name=resultJson.values.author[j].name
