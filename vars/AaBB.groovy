b8b7f2dbe3c3f8a7840cf19b01e1542f47b87709
REVATHISARAISWATHI <36625520+REVATHISARAISWATHI@users.noreply.github.com>
2020-02-25 17:59:13 +0530

    Update AaBB.groovy

--- vars/AaBB.groovy
+++ vars/AaBB.groovy
@@ -17,7 +17,7 @@ println(repoName)
 def jsonSlurper = new JsonSlurper()
 def resultJson = jsonSlurper.parse(new File("/var/lib/jenkins/workspace/${JOB_NAME}/output.json"))
 def total = resultJson.size
- echo "Total no.of commits in ${repoName} $total"
+ echo "Total no.of Pull reqests ${repoName} $total"
 def commiter=1
 List<String> JSON = new ArrayList<String>();
 
