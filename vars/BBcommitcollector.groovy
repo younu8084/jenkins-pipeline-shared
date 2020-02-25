1cf355d1ce94fb563d9238cbe44f24c27e687041
REVATHISARAISWATHI <36625520+REVATHISARAISWATHI@users.noreply.github.com>
2020-02-25 11:21:42 +0530

    Update BBcommitcollector.groovy

--- vars/BBcommitcollector.groovy
+++ vars/BBcommitcollector.groovy
@@ -19,7 +19,7 @@ println(repoName)
 def jsonSlurper = new JsonSlurper()
 def resultJson = jsonSlurper.parse(new File("/var/lib/jenkins/workspace/${JOB_NAME}/output.json"))
 def total = resultJson.size
-  echo "$total"
+ echo "Total no.of commits in '${repoName}' $total"
  
  }
 
