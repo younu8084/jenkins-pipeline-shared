787d5b97b014d656f716af77abea9f257f81f725
REVATHISARAISWATHI <36625520+REVATHISARAISWATHI@users.noreply.github.com>
2020-02-25 12:47:01 +0530

    Update AaBB.groovy

--- vars/AaBB.groovy
+++ vars/AaBB.groovy
@@ -13,28 +13,24 @@ def total = resultJson.size
   echo "$total"
 def value=resultJson.values.author[0].name
   echo "$value"
- // Date date = new Date() 
- //echo "$date"
  def time=resultJson.values[0].committerTimestamp
-   //data_date=$(date -d @`expr $time / 1000` +%Y-%m-%d)
-    //data_date=\"$data_date\"
    def name=resultJson.values.committer[0].name
-		email=resultJson.values.committer[0].emailAddress
+    def email=resultJson.values.committer[0].emailAddress
 		//dateArr=$dateArr$data_date,
    Date date = new Date(time) 
-   //SimpleDateFormat jdf = new SimpleDateFormat("yyyy-MM-dd HH:mm:ss z")
-   //jdf.setTimeZone(TimeZone.getTimeZone("GMT-4"))
-   //String data_date = jdf.format(date)
    
-		//echo $dateArr > dateData
-		echo "{\"commitDate\":$date,\"contributorsName\":"$name",\"contributorsEmail\":"$email"}," >> bitAllDataDb.json
+  sh "curl -X GET  -H -d  -u rig:rigaDapt@devOps http://18.224.68.30:7990/rest/api/1.0/projects/EDN/repos/rig/commits -o output.json"
 
-  def count=0
+
+//echo $dateArr > dateData
+echo "{\"commitDate\":$date,\"contributorsName\":"$name",\"contributorsEmail\":"$email"}," >> bitAllDataDb.json
+
+	def count=0
   //
  for(i=0;i<total;i++)
  {
    //if(resultJson.values.committerTimestamp[i]==1582522990000)
-   if (resultJson.values.committerTimestamp[i]==convdataTime)
+   if (resultJson.values.committerTimestamp[i]==date)
    {
     count ++
    }
