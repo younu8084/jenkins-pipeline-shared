6ea1408fc96dde1e5f5e48d400e1002c991b522d
REVATHISARAISWATHI <36625520+REVATHISARAISWATHI@users.noreply.github.com>
2020-02-25 11:43:06 +0530

    Update BBcommitcollector.groovy

--- vars/BBcommitcollector.groovy
+++ vars/BBcommitcollector.groovy
@@ -7,8 +7,8 @@ String a=jsonObj.scm.projects.project.repositories.repository.repo_name
 String repoName=a.replaceAll("\\[", "").replaceAll("\\]","");
 String b=jsonObj.scm.projects.project.project_key 
 String Key=b.replaceAll("\\[", "").replaceAll("\\]","");
-int count = jsonObj.config.emails.email.size()
-println("No of users"+ count)
+int ecount = jsonObj.config.emails.email.size()
+println("No of users "+ ecount)
 println(Key)
 println(repoName)
  Date date = new Date() 
@@ -19,9 +19,22 @@ def jsonSlurper = new JsonSlurper()
 def resultJson = jsonSlurper.parse(new File("/var/lib/jenkins/workspace/${JOB_NAME}/output.json"))
 def total = resultJson.size
  echo "Total no.of commits in ${repoName} $total"
- 
+ jq -c '.[]' output.json | while read i; do
 
- 
+		time=`echo $i | jq '.committerTimestamp'`
+
+		data_date=$(date -d @`expr $time / 1000` +%Y-%m-%d)
+
+		data_date=\"$data_date\"
+
+		name=`echo $i | jq '.committer.name'`
+
+		email=`echo $i | jq '.committer.emailAddress'`
+
+		dateArr=$dateArr$data_date,
+		echo $dateArr > dateData
+
+		echo "{\"commitDate\":$data_date,\"contributorsName\":"$name",\"contributorsEmail\":"$email"}," >> bitAllDataDb.json
  }
 
 
