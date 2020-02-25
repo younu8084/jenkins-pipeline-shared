4df09ecab036370e3356a3199e0383de87f9ca62
REVATHISARAISWATHI <36625520+REVATHISARAISWATHI@users.noreply.github.com>
2020-02-25 13:12:01 +0530

    Update AaBB.groovy

--- vars/AaBB.groovy
+++ vars/AaBB.groovy
@@ -3,7 +3,7 @@ import groovy.json.*
 def call(jsondata){
 def jsonString = jsondata
 def jsonObj = readJSON text: jsonString
-def resultJson = jsonSlurper.parse(new File("/var/lib/jenkins/workspace/${JOB_NAME}/ouput.json"))
+
  sh""" 
  curl -X GET  -H -d  -u rig:rigaDapt@devOps "http://18.224.68.30:7990/rest/api/1.0/projects/EDN/repos/rig/commits -o output.json"  
     """
@@ -12,6 +12,9 @@ def resultJson = jsonSlurper.parse(new File("/var/lib/jenkins/workspace/${JOB_NA
   //echo "$total"
 //def value=resultJson.values.author[0].name
  // echo "$value"
+	def jsonSlurper = new JsonSlurper()
+def resultJson = jsonSlurper.parse(new File("/var/lib/jenkins/workspace/${JOB_NAME}/output.json"))
+
  def time=resultJson.values[0].committerTimestamp
    def name=resultJson.values.committer[0].name
     def email=resultJson.values.committer[0].emailAddress
