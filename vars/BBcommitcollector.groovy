d9c4400b3cc3305e34e038fda49e38b5e4a07717
REVATHI <revathisaraiswathi@gmail.com>
2020-02-26 11:54:38 +0530

    Update BBcommitcollector.groovy

--- vars/BBcommitcollector.groovy
+++ vars/BBcommitcollector.groovy
@@ -40,7 +40,7 @@ String Property = "emailAddress";
 String commitsbyuser = _.groupBy(Array, Property);
 println(commitsbyuser)*/
 	 
-def n=groovy.json.JsonOutput.toJson(JSON.groupBy{it.emailAddress}.collect{['emailAddress':it.key]})
+def n=groovy.json.JsonOutput.toJson(JSON.groupBy{it.resultJson.values.author[j].emailAddress}.collect{['resultJson.values.author[j].emailAddress':it.key]})
 println(n)
   }
  
