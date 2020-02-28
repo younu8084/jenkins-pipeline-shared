1326217ede22365f53923ef12f0bad68ddec6705
REVATHISARAISWATHI <revathisaraiswathi@gmail.com>
2020-02-28 15:01:38 +0530

    Update BBcommitcollector.groovy

--- vars/BBcommitcollector.groovy
+++ vars/BBcommitcollector.groovy
@@ -31,10 +31,7 @@ for(i=0;i<ecount;i++)
 	     {
 	JSON.add(JsonOutput.toJson(resultJson.values[j]))
 	//println(JSON) 
-    GsonBuilder gsonBuilder = new GsonBuilder();
-    Gson gson = gsonBuilder.create();
-    String JSONObject = gson.toJson(JSON);
-    println(JSONObject)
+    def newObj=new Obj(JSON)
 		     
     }
 	
