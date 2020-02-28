98b7ae864bb5dcb6f872cf99fb4fdbbb1426cfde
REVATHISARAISWATHI <revathisaraiswathi@gmail.com>
2020-02-28 18:59:11 +0530

    Update BBcommitcollector.groovy

--- vars/BBcommitcollector.groovy
+++ vars/BBcommitcollector.groovy
@@ -22,7 +22,7 @@ def total = resultJson.size
 //def commiter=1
 List<String> JSON = new ArrayList<String>();
 List<String> JCOPY = new ArrayList<String>();
-Map<ImmutableList<String>, List<String>> map = new HashMap<ImmutableList<String>, List<String>>();
+Map<String, List<String>> map = new HashMap<String, List<String>>();
 for(i=0;i<ecount;i++)
 {	 
   for(j=0;j<total;j++)
@@ -41,7 +41,7 @@ for(i=0;i<ecount;i++)
 	 def count=JSON.size()
 	 //println(jsonObj.config.emails.email[i])
 	 JCOPY[i]=(JsonOutput.toJson(JSON))
-	 map.put(ImmutableList.of(jsonObj.config.emails.email[i],count),JCOPY[i])
+	 map.put(jsonObj.config.emails.email[i],JCOPY[i])
 	
 	
 	 JSON.clear()
