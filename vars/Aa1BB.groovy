9e99344c5b9c006a4ba1579ce21b4df60c26c58f
REVATHISARAISWATHI <revathisaraiswathi@gmail.com>
2020-02-28 14:54:17 +0530

    Update Aa1BB.groovy

--- vars/Aa1BB.groovy
+++ vars/Aa1BB.groovy
@@ -40,11 +40,12 @@ for(i=0;i<ecount;i++)
 	 {
    if(jsonObj.config.emails.email[i]==resultJson.values.author[j].emailAddress)
           {
-	   for(i=0;i<ecount;i++)
+	   //for(i=0;i<ecount;i++)
            if(JSON[i]==JSON[j+1])
 	      {
-                JSON.add(JsonOutput.toJson(resultJson.values[j]))
-		continue;
+                continue;
+		      JSON.add(JsonOutput.toJson(resultJson.values[j]))
+		
 	       }
 		  else 
 		  {
