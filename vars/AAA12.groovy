a73f6ed6b4ae1b4b73288d76d25f50d691b3ccab
REVATHISARAISWATHI <revathisaraiswathi@gmail.com>
2020-03-02 16:32:25 +0530

    Create AAA12.groovy

---
+++ vars/AAA12.groovy
@@ -0,0 +1,80 @@
+import groovy.json.*
+import groovy.json.JsonOutput
+
+def call(jsondata){
+def jsonString = jsondata
+def jsonObj = readJSON text: jsonString
+String a=jsonObj.scm.projects.project.repositories.repository.repo_name
+String repoName=a.replaceAll("\\[", "").replaceAll("\\]","");
+String b=jsonObj.scm.projects.project.project_key 
+String Key=b.replaceAll("\\[", "").replaceAll("\\]","");
+int ecount = jsonObj.config.emails.email.size()
+println("No of users "+ ecount)
+println(Key)
+println(repoName)
+// Date date = new Date() 
+ withCredentials([usernamePassword(credentialsId: 'bitbucket_cred', passwordVariable: 'pass', usernameVariable: 'userId')]) {
+  sh "curl -X GET  -H -d  -u $userId:$pass http://18.224.68.30:7990/rest/api/1.0/projects/'${Key}'/repos/'${repoName}'/commits -o output.json"
+ } 
+def jsonSlurper = new JsonSlurper()
+def resultJson = jsonSlurper.parse(new File("/var/lib/jenkins/workspace/${JOB_NAME}/output.json"))
+def total = resultJson.size
+echo "Total no.of commits in $repoName $total"
+//File file = new File(output.json)
+//file.write(total)
+//def commiter=1
+List<String> JSON = new ArrayList<String>();
+List<String> JCOPY = new ArrayList<String>();
+
+for(i=0;i<ecount;i++)
+{	 
+  for(j=0;j<total;j++)
+  {
+ if(jsonObj.config.emails.email[i]==resultJson.values.author[j].emailAddress)
+	     {
+	JSON.add(resultJson.values[j])
+	//println(JSON) 
+    
+		     
+    }
+	
+      
+  
+      }
+	 
+	
+	 count=JSON.size()
+	 //  println(USER)
+          
+	   JCOPY.add(["email":jsonObj.config.emails.email[i],"Individual_commit":JsonOutput.toJson(JSON),"Commit_count":count])
+	 //JCOPY[i]=JsonOutput.toJson(JSON)
+	 
+	String JSONObject = gson.toJson(JCOPY);
+	
+	 JSON.clear()
+	 
+
+	  
+}
+	
+
+def jsonBuilder = new groovy.json.JsonBuilder()
+
+jsonBuilder.bitbucket(
+  "total_commit": resultJson,
+ "commit_count": resultJson.size(),
+ "individual":JCOPY
+)
+
+
+//try{
+File file = new File("/var/lib/jenkins/workspace/${JOB_NAME}/commits.json")
+file.write(jsonBuilder.toPrettyString())	
+//}
+/*catch(IOException e)
+{
+	e.printStackTrace()
+}*/
+}
+
+
