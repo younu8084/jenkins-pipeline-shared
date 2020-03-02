0b5652c6b22e69ab3e8fe11ee1ccea3ce5497211
REVATHISARAISWATHI <revathisaraiswathi@gmail.com>
2020-03-02 12:54:04 +0530

    Update Bbitbucket.groovy

--- vars/Bbitbucket.groovy
+++ vars/Bbitbucket.groovy
@@ -65,14 +65,15 @@ for(i=0;i<JCOPY.size();i++)
 	 jsonBuilder.bitbucket(
  "totalcommit" : resultJson,
   "commit_cnt" : resultJson.size()
-	"user1" :JSONCOPY
-		 "user2" :JSONCOPY
-	 )
+   "user1" :JSONCOPY
+    "user2" :JSONCOPY
+	
 //def user1 = new user1(email:'rituparna.ghosh@wipro.com')
 //def user3 = new user3(email:'MO20090733@wipro.com')
 		 
 def indivudalcommits = [user1,user2]
 jsonBuilder(Indivudal:indivudalcommits)
+		  )
 //println("Using list of objects")
 //println(jsonBuilder.toPrettyString())
 println(jsonBuilder)
