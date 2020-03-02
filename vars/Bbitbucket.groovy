aad7998a16d1bcd6cae892285aa9c003e5012bd1
REVATHISARAISWATHI <revathisaraiswathi@gmail.com>
2020-03-02 10:58:31 +0530

    Create Bbitbucket.groovy

---
+++ vars/Bbitbucket.groovy
@@ -0,0 +1,56 @@
+import groovy.json.*
+
+def call(jsondata){
+def jsonString = jsondata
+def jsonObj = readJSON text: jsonString
+String a=jsonObj.scm.projects.project.repositories.repository.repo_name
+String repoName=a.replaceAll("\\[", "").replaceAll("\\]","");
+String b=jsonObj.scm.projects.project.project_key 
+String Key=b.replaceAll("\\[", "").replaceAll("\\]","");
+int ecount = jsonObj.config.emails.email.size()
+println("No of users "+ ecount)
+println(Key)
+println(repoName)
+// Date date = new Date() 
+ withCredentials([usernamePassword(credentialsId: 'bitbucket_cred', passwordVariable: 'pass', usernameVariable: 'userId')]) {
+  sh "curl -X GET  -H -d  -u $userId:$pass http://18.224.68.30:7990/rest/api/1.0/projects/'${Key}'/repos/'${repoName}'/commits -o output.json"
+ } 
+def jsonSlurper = new JsonSlurper()
+def resultJson = jsonSlurper.parse(new File("/var/lib/jenkins/workspace/${JOB_NAME}/output.json"))
+def total = resultJson.size
+ echo "Total no.of commits in ${repoName} $total"
+//def commiter=1
+List<String> JSON = new ArrayList<String>();
+List<String> JCOPY = new ArrayList<String>();
+Map<String, List<String>> map = new HashMap<String, List<String>>();
+for(i=0;i<ecount;i++)
+{	 
+  for(j=0;j<total;j++)
+  {
+ if(jsonObj.config.emails.email[i]==resultJson.values.author[j].emailAddress)
+	     {
+	JSON.add(resultJson.values[j])
+	//println(JSON) 
+    
+		     
+    }
+	
+      
+  
+      }
+	 //def count=JSON.size()
+	
+	 //println(jsonObj.config.emails.email[i])
+	 JCOPY[i]=(JsonOutput.toJson(JSON))
+	 map.put(jsonObj.config.emails.email[i],JCOPY[i])
+	
+	
+	 JSON.clear()
+	 
+
+	  
+}
+echo"map" >> output.json
+
+
+}
