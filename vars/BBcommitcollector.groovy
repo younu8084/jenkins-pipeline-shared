79b0048e801ea4a9f156b9af9cc485317d1d37bc
REVATHISARAISWATHI <revathisaraiswathi@gmail.com>
2020-03-02 13:29:35 +0530

    Update BBcommitcollector.groovy

--- vars/BBcommitcollector.groovy
+++ vars/BBcommitcollector.groovy
@@ -61,7 +61,8 @@ jsonBuilder.bitbucket(
   "total_commit": resultJson,
  "commit_count": resultJson.size()
 )
-def inputFile = new File("/var/lib/jenkins/workspace/${JOB_NAME}/totalcommits.json")	
+def inputFile = new File("/var/lib/jenkins/workspace/${JOB_NAME}/totalcommits.json")
+inputFile.createNewFile() 
 //sh "echo '$jsonBuilder' >>totalcommit.json" 
 inputFile.write(jsonBuilder)
 	
