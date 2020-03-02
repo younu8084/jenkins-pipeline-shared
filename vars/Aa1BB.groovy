8034c92161c6dc4054513e91b2f1881e05b439e3
REVATHISARAISWATHI <revathisaraiswathi@gmail.com>
2020-03-02 18:03:10 +0530

    Update Aa1BB.groovy

--- vars/Aa1BB.groovy
+++ vars/Aa1BB.groovy
@@ -36,7 +36,7 @@ for(i=0;i<ecount;i++)
 def jsonBuilder = new groovy.json.JsonBuilder()
 
 jsonBuilder.bitbucket(
-  "total_pullrequesr": resultJson,
+  "total_pullrequests": resultJson,
  "pullreq_count": resultJson.size()
 )
 File file = new File("/var/lib/jenkins/workspace/${JOB_NAME}/pulls.json")
