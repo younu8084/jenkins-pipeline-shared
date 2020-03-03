6a35df02a03d5ac9effab55d6d528716872d5d30
REVATHISARAISWATHI <revathisaraiswathi@gmail.com>
2020-03-03 11:31:47 +0530

    Update BBcommitcollector.groovy

--- vars/BBcommitcollector.groovy
+++ vars/BBcommitcollector.groovy
@@ -48,8 +48,8 @@ for(i=0;i<ecount;i++)
 	 count=JSON.size()
 	 //  println(USER)
          	
-
-	   JCOPY.add(["Email":jsonObj.config.emails.email[i],"Individual_commit":JsonOutput.toString(JSON),"Commit_count":count])
+String json = JsonOutput.toJson(JSON)
+	   JCOPY.add(["Email":jsonObj.config.emails.email[i],"Individual_commit":json,"Commit_count":count])
 	 //JCOPY[i]=JsonOutput.toJson(JSON)
 	 
 	
