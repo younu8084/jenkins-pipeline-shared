b70b9ae9ac2dd395bb8bf3e3fb3c0fd4c7264adc
REVATHISARAISWATHI <revathisaraiswathi@gmail.com>
2020-03-03 11:07:31 +0530

    Update AAA12.groovy

--- vars/AAA12.groovy
+++ vars/AAA12.groovy
@@ -54,7 +54,7 @@ for(i=0;i<ecount;i++)
 	  
 }
 String c=jsonObj.config.individual.Individual_commit
-String JCOPY=c.replaceAll("\","").replaceAll("\","");
+String JCOPY=c.replaceAll("\"," ");
 	// GsonBuilder gsonBuilder = new GsonBuilder();
 	//	Gson gson = gsonBuilder.create();
  
