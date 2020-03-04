09117d888cc91b4de650d2664ca2b55e0d06e6dc
REVATHISARAISWATHI <revathisaraiswathi@gmail.com>
2020-03-04 16:51:33 +0530

    Update Score.groovy

--- vars/Score.groovy
+++ vars/Score.groovy
@@ -4,9 +4,17 @@ def call(jsondata,bitbucket){
 def jsonString = jsondata
 def jsonObj = readJSON text: jsonString
 int ecount = jsonObj.config.emails.email.size()
+//List<String> jsonStringa= new ArrayList<String>();
+  
 def jsonStringa = bitbucket
 def jsonObja = readJSON text: jsonStringa
 int total=jsonObja.bitbucket.Commit_count
   println(jsonObja)
   println(total)
+  int score=1
+  if(total>5)
+  {
+    score=score*10
+  }
+  println(score)
 }
