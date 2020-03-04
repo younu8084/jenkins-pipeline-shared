127ac9d473e4977fb312c60e4ebd69e26cd5d3e3
REVATHISARAISWATHI <revathisaraiswathi@gmail.com>
2020-03-04 16:06:58 +0530

    Update Score.groovy

--- vars/Score.groovy
+++ vars/Score.groovy
@@ -1,10 +1,10 @@
 import groovy.json.*
 
 def call(jsondata,bitbucket){
-def jsonString = jsondata
+/*def jsonString = jsondata
 def jsonObj = readJSON text: jsonString
 int ecount = jsonObj.config.emails.email.size()
 def jsonStringa = bitbucket
 def jsonObja = readJSON text: jsonStringa
-println(jsonObja)
+println(jsonObja)*/
 }
