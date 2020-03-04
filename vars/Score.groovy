5981f38b1466b5aeb7a8f340e415cdf36afa6cd1
REVATHISARAISWATHI <revathisaraiswathi@gmail.com>
2020-03-04 15:48:53 +0530

    Update Score.groovy

--- vars/Score.groovy
+++ vars/Score.groovy
@@ -1,23 +1,10 @@
 import groovy.json.*
 
-def call(jsondata){
+def call(jsondata,bitbucket){
 def jsonString = jsondata
 def jsonObj = readJSON text: jsonString
 int ecount = jsonObj.config.emails.email.size()
-def jsonSlurper = new JsonSlurper()
-def resultJson = jsonSlurper.parse(new File("/var/lib/jenkins/workspace/${JOB_NAME}/commits1.json"))
-int total=resultJson.bitbucket.Commit_count
-for(i=0;i<ecount;i++)
-{	 
-  for(j=0;j<total;j++)
-  {
- if(jsonObj.config.emails.email[i]==resultJson.bitbucket.Total_commits[i].author.emailAddress)
-	     {
-	println("HI")
-    }
-
-}
-}
-
-
+def jsonStringa = bitbucket
+def jsonObja = readJSON text: jsonStringa
+println(jsonObja)
 }
