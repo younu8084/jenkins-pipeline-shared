11b6d9f9e69ec376594a4859d51b354bb384725e
REVATHISARAISWATHI <revathisaraiswathi@gmail.com>
2020-03-04 16:18:27 +0530

    Update Score.groovy

--- vars/Score.groovy
+++ vars/Score.groovy
@@ -6,5 +6,5 @@ def jsonObj = readJSON text: jsonString
 int ecount = jsonObj.config.emails.email.size()
 def jsonStringa = bitbucket
 def jsonObja = readJSON text: jsonStringa
-println(jsonObja)
+println(jsonObja.bitbucket.Commit_count)
 }
