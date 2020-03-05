092624bca0605d53f020f0cbb22b708cb1f1a95d
REVATHISARAISWATHI <revathisaraiswathi@gmail.com>
2020-03-05 11:21:30 +0530

    Update Score.groovy

--- vars/Score.groovy
+++ vars/Score.groovy
@@ -1,11 +1,22 @@
 import groovy.json.*
 
-def call(jsondata,bitbucket){
+def call(jsondata,bitbucket,bamboo){
 def jsonString = jsondata
 def jsonObj = readJSON text: jsonString
 int ecount = jsonObj.config.emails.email.size()
-//List<String> jsonStringa= new ArrayList<String>();
+List<String> jsonStringa= new ArrayList<String>();
+  jsonStringa.add(bitbucket)
+   jsonStringa.add(bamboo)
+/*def jsonStringa = bitbucket
+def jsonObja = readJSON text: jsonStringa
   
+def jsonStringb = bamboo
+def jsonObjb = readJSON text: jsonStringb*/
+ 
+for(i=0;i<jsonStringa.size();i++)
+  { 
+if(jsonStringa[i]==bitbucket)
+    {
 def jsonStringa = bitbucket
 def jsonObja = readJSON text: jsonStringa
 int total=jsonObja.bitbucket.Commit_count
@@ -15,6 +26,23 @@ int total=jsonObja.bitbucket.Commit_count
   if(total>5)
   {
     score=score*10
+  }
+  }
+   if(jsonStringa[i]==bamboo)
+    {
+      def jsonStringa = bamboo
+def jsonObj = readJSON text: jsonString
+  //println(jsonObj)
+  def cnt =jsonObj.Bamboo.teamsuccessbuild_cnt
+ // def res=bamboo1.bamboo.teamsuccessbuild_cnt
+ // def obj = JSON.parse(bamboo1)
+ println(cnt)
+ def score=1
+ if(cnt>10)
+  {
+   score=1*10 
+  }
+    }
   }
   println(score)
 }
