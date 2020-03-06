fc21ff0d79873cb37bdd08f3e0d6957e2e86de22
REVATHISARAISWATHI <revathisaraiswathi@gmail.com>
2020-03-06 15:22:35 +0530

    Update gamefication1.groovy

--- vars/gamefication1.groovy
+++ vars/gamefication1.groovy
@@ -1,34 +1,33 @@
 import groovy.json.*
 import groovy.json.JsonOutput
-def call(jsondata,bamboo,bitbucket)
+def call(jsondata,bamboo1)
 {
 def jsonString = jsondata
 def jsonObj = readJSON text: jsonString
 int ecount = jsonObj.config.emails.email.size()
-List<String> jsonStringa= new ArrayList<String>();
-  jsonStringa.add(bitbucket)
-   jsonStringa.add(bamboo)
-   //jsonStringa.add(gitlab)
+def team=jsonObj.riglet_info.name
+//println(bamboo1)
+	def jsonString1 = bamboo1
+        def jsonObj1 = readJSON text: jsonString1
+       //  println(jsonObj1)
+
  
  List<String> JSON = new ArrayList<String>();
   List<String> LIST = new ArrayList<String>();
   List<String> JSON1 = new ArrayList<String>();
+  def jsonBuilder = new groovy.json.JsonBuilder()
+	
 
-  for(j=0;j<jsonStringa.size();j++)
+  for(j=0;j<ecount;j++)
    {
 	 def email=jsonObj.config.emails.email[j] 
-	
-	 if(jsonStringa[j].contains("Bamboo"))
-    {   
+	   
+	  //println(email)  
      // name="Bamboo"
-    //  def jsonStringb = bamboo
-def jsonObjb = readJSON text: jsonStringa[j]
-for(i=0;i<ecount;i++)
-	   {
-  //println(jsonObj)
-  def scnt =jsonObjb.Bamboo.individualsuccess[i].Success_cnt
-  def fcnt =jsonObjb.Bamboo.individualfailure[i].Failure_cnt
- def email1=jsonObjb.Bamboo.individualsuccess[i].email
+      
+  def scnt =jsonObj1.Bamboo.individualsuccess[j].Success_cnt
+  def fcnt =jsonObj1.Bamboo.individualfailure[j].Failure_cnt
+ def email1=jsonObj1.Bamboo.individualsuccess[j].email
       
  // def res=bamboo1.bamboo.teamsuccessbuild_cnt
  // def obj = JSON.parse(bamboo1)
@@ -48,33 +47,18 @@ for(i=0;i<ecount;i++)
     LIST.add(["metric":"No of least failure builds","score":score])
 	  reward=reward+score 
     score=0
-  }}
-	   }
-  if(jsonStringa[j].contains("bitbucket"))
-    {
-    def jsonObja = readJSON text: jsonStringa[j]
- int total=jsonObja.bitbucket.Commit_count
-	    for(i=0;i<total;i++)
-	    {
-int total1=jsonObja.bitbucket.Individual_commits[i].Commit_count
-def email1=jsonObja.bitbucket.Individual_commits[i].Email
-    int score=0
-	   int reward=0
- if(email==email1 && total1>2)
-  {
-   score=score+10 
-    LIST.add(["metric":"Commits Score","score":score])
-	reward=reward+score  
-    score=0
   }
-	    }
-    }
 	   JSON1=LIST.clone()
 	   
    JSON.add(["email":email,"reward": reward,"metrics":JSON1])
     LIST.clear()
     }
+	 jsonBuilder(
+		 "team":team,
+		 "metrics":JSON
+  
+  )
      
     
-  println(JSON)
+  println(jsonBuilder)
 }
