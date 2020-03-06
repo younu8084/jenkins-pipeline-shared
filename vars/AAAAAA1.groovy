d1f43ba935f4aa37dd4e9a2d1eddf5bfc20cab91
REVATHISARAISWATHI <revathisaraiswathi@gmail.com>
2020-03-06 15:52:28 +0530

    Update AAAAAA1.groovy

--- vars/AAAAAA1.groovy
+++ vars/AAAAAA1.groovy
@@ -1,75 +1,119 @@
 import groovy.json.*
-
-
-def call(jsondata){
+import groovy.json.JsonOutput
+def call(jsondata,bamboo,bitbucket,gitlab)
+{
 def jsonString = jsondata
 def jsonObj = readJSON text: jsonString
-String a=jsonObj.scm.projects.project.repositories.repository.repo_name
-String repoName=a.replaceAll("\\[", "").replaceAll("\\]","");
-String b=jsonObj.scm.projects.project.project_key 
-String Key=b.replaceAll("\\[", "").replaceAll("\\]","");
 int ecount = jsonObj.config.emails.email.size()
-println("No of users "+ ecount)
-println(Key)
-println(repoName)
-// Date date = new Date() 
- withCredentials([usernamePassword(credentialsId: 'bitbucket_cred', passwordVariable: 'pass', usernameVariable: 'userId')]) {
-  sh "curl -X GET  -H -d  -u $userId:$pass http://18.224.68.30:7990/rest/api/1.0/projects/'${Key}'/repos/'${repoName}'/commits -o outputbitbucket.json"
- } 
+	def team=jsonObj.riglet_info.name
 
-def jsonSlurper = new JsonSlurper()
-def resultJson = jsonSlurper.parse(new File("/var/lib/jenkins/workspace/${JOB_NAME}/outputbitbucket.json"))
-def total = resultJson.size
-echo "Total no.of commits in $repoName $total"
-//File file = new File(output.json)
-//file.write(total)
-//def commiter=1
-List<String> JSON = new ArrayList<String>();
-List<String> JCOPY = new ArrayList<String>();
-List<String> JSON1= new ArrayList<String>();
-for(i=0;i<ecount;i++)
-{	 
-  for(j=0;j<total;j++)
+ 
+ List<String> JSON = new ArrayList<String>();
+  List<String> LIST = new ArrayList<String>();
+  List<String> JSON1 = new ArrayList<String>();
+	List<String> jsonStringa= new ArrayList<String>();
+	jsonStringa.add(bamboo)
+  jsonStringa.add(bitbucket)
+   jsonStringa.add(gitlab)
+	 for(j=0;j<ecount;j++)
+   {
+	 def email=jsonObj.config.emails.email[j] 
+	   int score=0
+    int reward=0
+    String name="  "
+	 for(i=0;i<jsonStringa.size();i++)
+  { 
+   
+	  
+  if(jsonStringa[i].contains("Bamboo"))
+    {
+ 
+	   
+	    
+     name="bamboo"
+    //  def jsonStringb = bamboo
+	   // def jsonString1 = jsonStringa[i]
+	   def jsonObja = readJSON text: jsonStringa[i]
+
+  //println(jsonObja)
+  def scnt =jsonObja.Bamboo.individualsuccess[j].Success_cnt
+  def fcnt =jsonObja.Bamboo.individualfailure[j].Failure_cnt
+ def email1=jsonObja.Bamboo.individualsuccess[j].email
+      
+ // def res=bamboo1.bamboo.teamsuccessbuild_cnt
+ // def obj = JSON.parse(bamboo1)
+ println(scnt)
+ //int score=0
+	  
+ if(email==email1 && scnt>1)
   {
-
- if(jsonObj.config.emails.email[i]==resultJson.values.author[j].emailAddress)
-	     {
-	JSON.add(resultJson.values[j])
-	//println(JSON) 
+   score=scnt*1
+    LIST.add(["toolname":name,"metric":"No of more successful builds","score":score])
+	reward=reward+score  
+    score=0
+  }
+     if(email==email1 && fcnt<2 && scnt!=0)
+  {
+   score=score+10 
+    LIST.add(["toolname":name,"metric":"No of least failure builds","score":score])
+	  reward=reward+score 
+    score=0
+  }
+   }
+	    if(jsonStringa[i].contains("bitbucket"))
+    {
+      name="bitbucket"
+	    
+	    
+//def jsonStringa = bitbucket
+def jsonObjb = readJSON text: jsonStringa[i]
+int total=jsonObjb.bitbucket.Individual_commits[j].Commit_count
+	    def email1=jsonObjb.bitbucket.Individual_commits[j].Email
+ // println(jsonObja)
+  //println(total)
+ 
+  if(email==email1 && total>1)
+  {
+    score=total*1
+	   LIST.add(["toolname":name,"metric":"commits","score":score])
+	  reward=reward+score
+	  score=0
+  }
+  }
+	  if(jsonStringa[i].contains("gitlab"))
+      {
+        name="gitlab"
+	      //metric="commits"
+        def jsonObjc= readJSON text: jsonStringa[i]
+  //println(jsonObj)
+  def cnt =jsonObjc.gitlab.individual_commit_Details[j].Commit_cnt
+	       def email1=jsonObjc.gitlab.individual_commit_Details[j].email
+	       if(email==email1 && cnt>1)
+  {
+    score=cnt*1
+	   LIST.add(["toolname":name,"metric":"commits","score":score])
+	  reward=reward+score
+	  score=0
+  }
+      }
     }
-}
-	 count=JSON.size()
-	 //  println(USER)
-         	
- JSON1[i]=JSON.clone()
- JCOPY.add(["Email":jsonObj.config.emails.email[i],"Individual_commit":JSON1[i],"Commit_count":count])
-	
-JSON.clear()
-	int score =0;
-	if(count>1)
-	{
-	score=score+10;
-	}
-	
-}
-	println(score)
-def list = JCOPY.sort()
-	
-//int score =0;
-//	if(count>3)
-//	{
-//	score=score+10;
-//	}
-def jsonBuilder = new groovy.json.JsonBuilder()
+	   JSON1[j]=LIST.clone()
+	   
+   JSON.add(["email":email,"reward": reward,"metrics":JSON1[j]])
+    LIST.clear()
+	reward=0    
+    }
+	 def list =JSON.sort()
+     def jsonBuilder = new groovy.json.JsonBuilder()
 
-jsonBuilder.bitbucket(
-  "Total_commits": resultJson.values,
- "Commit_count": resultJson.size,
- "Individual_commits":JCOPY
-)
-
-
-File file = new File("/var/lib/jenkins/workspace/${JOB_NAME}/commitsbitbucket.json")
-file.write(jsonBuilder.toPrettyString())	
-//return jsonBuilder
+jsonBuilder(
+ "teamName":team,
+  "metrics" : JSON
+  
+) 
+ 
+  File file = new File("/var/lib/jenkins/workspace/${JOB_NAME}/game.json")
+file.write(jsonBuilder.toPrettyString())
+    
+  //println(JSON)
 }
